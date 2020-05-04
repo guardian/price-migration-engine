ca4c317db9f6c463fe15695e7a16b0175ab77e3b
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-05-04 16:02:11 +0100

    Determine next billing date using invoice preview

--- src/main/scala/pricemigrationengine/model/ZuoraRatePlan.scala
+++ src/main/scala/pricemigrationengine/model/ZuoraRatePlan.scala
@@ -1,8 +1,14 @@
 package pricemigrationengine.model
 
+import upickle.default.{ReadWriter, macroRW}
+
 case class ZuoraRatePlan(
     productName: String,
     ratePlanName: String,
     ratePlanCharges: List[ZuoraRatePlanCharge] = Nil,
     lastChangeType: Option[String] = None
 )
+
+object ZuoraRatePlan {
+  implicit val rw: ReadWriter[ZuoraRatePlan] = macroRW
+}
