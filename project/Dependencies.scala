86eedec7cfb949f79d7172c0d776322c30ae652c
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-05-05 14:58:03 +0100

    Add lambda core

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,6 @@ import sbt._
 object Dependencies {
   lazy val zio = "dev.zio" %% "zio" % "1.0.0-RC18-2"
   lazy val upickle = "com.lihaoyi" %% "upickle" % "1.1.0"
+  lazy val lambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.5"
 }
--- src/main/scala/pricemigrationengine/handlers/EstimationHandler.scala
+++ src/main/scala/pricemigrationengine/handlers/EstimationHandler.scala
@@ -2,24 +2,20 @@ package pricemigrationengine.handlers
 
 import java.time.LocalDate
 
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import pricemigrationengine.model.CohortTableFilter.ReadyForEstimation
 import pricemigrationengine.model._
 import pricemigrationengine.services.{CohortTable, CohortTableTest, Zuora, ZuoraTest}
 import zio.console.Console
-import zio.{App, ZEnv, ZIO, console}
+import zio.{App, Runtime, ZEnv, ZIO, console}
 
-object EstimationHandler extends App {
+object EstimationHandler extends App with RequestHandler[Unit, Unit] {
 
   // TODO: configuration
   val batchSize = 100
   val earliestStartDate = LocalDate.now
 
-  val main: ZIO[Console with CohortTable with Zuora, Failure, Unit] =
-    for {
-      cohortItems <- CohortTable.fetch(ReadyForEstimation, batchSize)
-      results <- ZIO.foreach(cohortItems)(item => estimation(item, earliestStartDate))
-      _ <- ZIO.foreach(results)(CohortTable.update)
-    } yield ()
+  private val runtime = Runtime.default
 
   def estimation(item: CohortItem, earliestStartDate: LocalDate): ZIO[Zuora, Failure, EstimationResult] = {
     val result = for {
@@ -29,13 +25,20 @@ object EstimationHandler extends App {
     result.absolve
   }
 
+  val main: ZIO[Console with CohortTable with Zuora, Failure, Unit] =
+    for {
+      cohortItems <- CohortTable.fetch(ReadyForEstimation, batchSize)
+      results <- ZIO.foreach(cohortItems)(item => estimation(item, earliestStartDate))
+      _ <- ZIO.foreach(results)(CohortTable.update)
+    } yield ()
+
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
     main
-      .provideCustomLayer(
-        CohortTableTest.impl ++ ZuoraTest.impl
-      )
+      .provideCustomLayer(CohortTableTest.impl ++ ZuoraTest.impl)
       .foldM(
         e => console.putStrLn(s"Failed: $e") *> ZIO.succeed(1),
         _ => ZIO.succeed(0)
       )
+
+  def handleRequest(unused: Unit, context: Context): Unit = runtime.unsafeRun(run(Nil))
 }
