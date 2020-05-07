185b76bcbe12a68e28ab6a047437823b4d8207ec
Francis Rhys-Jones <francis@rhys-jones.com>
2020-05-07 17:54:12 +0100

    Implementation for update item in CohortTable

--- src/main/scala/pricemigrationengine/dynamodb/DynamoDBZIO.scala
+++ src/main/scala/pricemigrationengine/dynamodb/DynamoDBZIO.scala
@@ -3,22 +3,25 @@ package pricemigrationengine.dynamodb
 import java.util
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.model.{AttributeValue, QueryRequest, QueryResult}
+import com.amazonaws.services.dynamodbv2.model._
 import pricemigrationengine.services.Logging
-import zio.console.Console
 import zio.stream.ZStream
-import zio.{IO, UIO, ZIO, ZLayer}
+import zio.{IO, ZIO, ZLayer}
 
 import scala.jdk.CollectionConverters._
 
 case class DynamoDBZIOError(message: String)
 
+trait DynamoDBSerialiser[A] { def serialise(value: A): java.util.Map[String, AttributeValue] }
+trait DynamoDBUpdateSerialiser[A] { def serialise(value: A): java.util.Map[String, AttributeValueUpdate] }
+trait DynamoDBDeserialiser[A] { def deserialise(value: java.util.Map[String, AttributeValue]): IO[DynamoDBZIOError, A]  }
+
 object DynamoDBZIO {
   trait Service {
-    def query[A](
-      query: QueryRequest,
-      deserializer: java.util.Map[String, AttributeValue] => IO[DynamoDBZIOError, A]
-    ): ZStream[Any, DynamoDBZIOError, A]
+    def query[A](query: QueryRequest)(implicit deserializer: DynamoDBDeserialiser[A]): ZStream[Any, DynamoDBZIOError, A]
+    def update[A, B](table: String, key: A, value: B)
+                    (implicit keySerializer: DynamoDBSerialiser[A],
+                     valueSerializer: DynamoDBUpdateSerialiser[B]): IO[DynamoDBZIOError, Unit]
   }
 
   val impl:  ZLayer[DynamoDBClient with Logging, Nothing, DynamoDBZIO] =
@@ -28,15 +31,15 @@ object DynamoDBZIO {
         private val amazonDynamoDB = dependencies.get[AmazonDynamoDB]
 
         override def query[A](
-          query: QueryRequest,
-          deserializer: java.util.Map[String, AttributeValue] => IO[DynamoDBZIOError, A]
-        ): ZStream[Any, DynamoDBZIOError, A] =
-          recursivelyExecuteQueryUntilAllResultsAreStreamed(query).mapM(deserializer)
+          query: QueryRequest
+        )(implicit deserializer: DynamoDBDeserialiser[A]): ZStream[Any, DynamoDBZIOError, A] =
+          recursivelyExecuteQueryUntilAllResultsAreStreamed(query)
+            .mapM(deserializer.deserialise)
 
         private def recursivelyExecuteQueryUntilAllResultsAreStreamed[A](
           query: QueryRequest
         ): ZStream[Any, DynamoDBZIOError, util.Map[String, AttributeValue]] = {
-          ZStream.unfoldM(Some(query).asInstanceOf[Option[QueryRequest]]) {
+          ZStream.unfoldM(Some(query): Option[QueryRequest]) {
               case Some(queryRequest) =>
                 for {
                   queryResult <- sendQueryRequest(queryRequest)
@@ -56,6 +59,23 @@ object DynamoDBZIO {
               amazonDynamoDB.query(queryRequest)
             }
             .mapError(ex => DynamoDBZIOError(s"Failed to execute query $queryRequest : $ex"))
+        }
+
+        def update[A, B](table: String, key: A, value: B)
+                        (implicit keySerializer: DynamoDBSerialiser[A],
+                                  valueSerializer: DynamoDBUpdateSerialiser[B]): IO[DynamoDBZIOError, Unit] = {
+          ZIO
+            .effect {
+              amazonDynamoDB.updateItem(
+                new UpdateItemRequest(
+                  table,
+                  keySerializer.serialise(key),
+                  valueSerializer.serialise(value)
+                )
+              )
+              ()
+            }
+            .mapError(ex => DynamoDBZIOError(s"Failed to write value '$value' to '$table': $ex"))
         }
       }
     }
--- src/main/scala/pricemigrationengine/services/CohortTable.scala
+++ src/main/scala/pricemigrationengine/services/CohortTable.scala
@@ -1,15 +1,19 @@
 package pricemigrationengine.services
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util
 
-import com.amazonaws.services.dynamodbv2.model.{AttributeValue, QueryRequest}
-import pricemigrationengine.dynamodb.{DynamoDBZIO, DynamoDBZIOError}
+import com.amazonaws.services.dynamodbv2.model.{AttributeAction, AttributeValue, AttributeValueUpdate, QueryRequest}
+import pricemigrationengine.dynamodb._
+import pricemigrationengine.model.CohortTableFilter.EstimationComplete
 import pricemigrationengine.model._
 import zio.stream.ZStream
 import zio.{IO, UIO, ZIO, ZLayer}
 
 import scala.jdk.CollectionConverters._
 
+case class CohortTableKey(subscriptionNumber: String)
 
 object CohortTable {
 
@@ -28,31 +32,60 @@ object CohortTable {
   def update(result: EstimationResult): ZIO[CohortTable, CohortUpdateFailure, Unit] =
     ZIO.accessM(_.get.update(result))
 
+  private implicit val cohortItemDeserialiser: DynamoDBDeserialiser[CohortItem] =
+    cohortItem =>
+      getStringFromResults(cohortItem, "subscriptionNumber")
+        .map(CohortItem.apply)
+
+  private implicit val estimationResultSerialiser: DynamoDBUpdateSerialiser[EstimationResult] =
+    estimationResult =>
+      Map(
+        stringFieldUpdate("processingStage", EstimationComplete.value),
+        dateFieldUpdate("expectedStartDate", estimationResult.expectedStartDate),
+        doubleFieldUpdate("estimatedNewPrice", estimationResult.estimatedNewPrice)
+      ).asJava
+
+  private implicit val cohortTableKeySerialiser: DynamoDBSerialiser[CohortTableKey] =
+    estimationResult =>
+      Map(stringUpdate("subscriptionNumber", estimationResult.subscriptionNumber)).asJava
+
+  private def stringFieldUpdate(fieldName: String, stringValue: String) =
+    fieldName -> new AttributeValueUpdate(new AttributeValue().withS(stringValue), AttributeAction.PUT)
+
+  private def dateFieldUpdate(fieldName: String, dateValue: LocalDate) =
+    fieldName -> new AttributeValueUpdate(new AttributeValue().withS(dateValue.format(DateTimeFormatter.ISO_LOCAL_DATE)), AttributeAction.PUT)
+
+  private def doubleFieldUpdate(fieldName: String, doubleValue: Double) =
+    fieldName -> new AttributeValueUpdate(new AttributeValue().withS(doubleValue.toString), AttributeAction.PUT)
+
+  private def stringUpdate(fieldName: String, stringValue: String) =
+    fieldName -> new AttributeValue().withS(stringValue)
+
   val impl: ZLayer[DynamoDBZIO, Nothing, UserRepo] =
-    ZLayer.fromFunction { dynamoDBZStream: DynamoDBZIO =>
+    ZLayer.fromFunction { dynamoDBZIO: DynamoDBZIO =>
       new Service {
         override def fetch(filter: CohortTableFilter, batchSize: Int): IO[CohortFetchFailure, ZStream[Any, CohortFetchFailure, CohortItem]] = {
           UIO(
-            dynamoDBZStream.get.query(
-              new QueryRequest()
-                .withTableName("PriceMigrationEngineDev")
-                .withIndexName("ProcessingStageIndex")
-                .withKeyConditionExpression("processingStage = :processingStage")
-                .withExpressionAttributeValues(
-                  Map(":processingStage" -> new AttributeValue(filter.value)).asJava
-                )
-                .withLimit(batchSize),
-              { result =>
-                getStringFromResults(result, "subscriptionNumber").map(CohortItem.apply)
-              }
-            ).mapError(error => CohortFetchFailure(error.toString))
+            dynamoDBZIO
+              .get
+              .query(
+                new QueryRequest()
+                  .withTableName("PriceMigrationEngineDev")
+                  .withIndexName("ProcessingStageIndex")
+                  .withKeyConditionExpression("processingStage = :processingStage")
+                  .withExpressionAttributeValues(
+                    Map(":processingStage" -> new AttributeValue(filter.value)).asJava
+                  )
+                  .withLimit(batchSize)
+              ).mapError(error => CohortFetchFailure(error.toString))
           )
         }
 
         override def update(result: EstimationResult): ZIO[Any, CohortUpdateFailure, Unit] =
-          for {
-            fakeResult <- ZIO.effect(()).mapError(_ => CohortUpdateFailure(""))
-          } yield fakeResult
+          dynamoDBZIO
+            .get
+            .update("PriceMigrationEngineDev", CohortTableKey(result.subscriptionName), result)
+            .mapError(error => CohortUpdateFailure(error.toString))
       }
     }
 
