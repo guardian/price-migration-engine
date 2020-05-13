f66d6c4816d96f1ccc0d7725ed3f40f8016acf4a
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-05-13 14:23:38 +0100

    Estimate discounted changes

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   private val zioVersion = "1.0.0-RC18-2"
 
-  lazy val awsDynamoDb = "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.780"
+  lazy val awsDynamoDb = "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.781"
   lazy val zio = "dev.zio" %% "zio" % zioVersion
   lazy val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
   lazy val upickle = "com.lihaoyi" %% "upickle" % "1.1.0"
--- src/test/scala/pricemigrationengine/model/AmendmentDataTest.scala
+++ src/test/scala/pricemigrationengine/model/AmendmentDataTest.scala
@@ -25,13 +25,13 @@ class AmendmentDataTest extends munit.FunSuite {
   private def invoiceListFromJson(resource: String): ZuoraInvoiceList =
     instanceFromJson[ZuoraInvoiceList](resource)
 
-  test("billing date is first after migration start date") {
+  test("nextBillingDate: billing date is first after migration start date") {
     val invoiceList = invoiceListFromJson("InvoicePreview.json")
     val billingDate = AmendmentData.nextBillingDate(invoiceList, after = migrationStartDate)
     assertEquals(billingDate, Right(LocalDate.of(2021, 1, 8)))
   }
 
-  test("billing date calculation fails if there are no invoices after migration start date") {
+  test("nextBillingDate: calculation fails if there are no invoices after migration start date") {
     val invoiceList = invoiceListFromJson("InvoicePreviewTermEndsBeforeMigration.json")
     val billingDate = AmendmentData.nextBillingDate(invoiceList, after = migrationStartDate)
     assertEquals(
@@ -40,13 +40,56 @@ class AmendmentDataTest extends munit.FunSuite {
     )
   }
 
-  test("price data calculation is correct for a monthly voucher subscription") {
+  test("priceData: calculation is correct for a monthly voucher subscription") {
     val priceData = AmendmentData.priceData(
-      pricing = productPricingMap(productCatalogueFromJson("Catalogue2.json")),
+      pricingData = productPricingMap(productCatalogueFromJson("Catalogue2.json")),
       subscription = subscriptionFromJson("Monthly2.json"),
       invoiceList = invoiceListFromJson("InvoicePreview2.json"),
       startDate = LocalDate.of(2020, 5, 28)
     )
     assertEquals(priceData, Right(PriceData(currency = "GBP", oldPrice = 47.62, newPrice = 52.99)))
+  }
+
+  test("priceData: calculation is correct for a monthly discounted voucher subscription") {
+    val priceData = AmendmentData.priceData(
+      pricingData = productPricingMap(productCatalogueFromJson("Catalogue3.json")),
+      subscription = subscriptionFromJson("MonthlyDiscounted3.json"),
+      invoiceList = invoiceListFromJson("InvoicePreview3.json"),
+      startDate = LocalDate.of(2020, 6, 15)
+    )
+    assertEquals(priceData, Right(PriceData(currency = "GBP", oldPrice = 10.38, newPrice = 10.99)))
+  }
+
+  test("combinePrices: combines prices correctly") {
+    val combinedPrice = AmendmentData.combinePrices(
+      Seq(
+        ZuoraPricing("GBP", Some(11), 0),
+        ZuoraPricing("GBP", Some(10.99), 0)
+      )
+    )
+    assertEquals(combinedPrice.toDouble, 21.99)
+  }
+
+  test("combinePrices: combines prices and discount correctly") {
+    val combinedPrice = AmendmentData.combinePrices(
+      Seq(
+        ZuoraPricing("GBP", Some(11), 0),
+        ZuoraPricing("GBP", Some(10.99), 0),
+        ZuoraPricing("", None, 50)
+      )
+    )
+    assertEquals(combinedPrice.toDouble, 10.99)
+  }
+
+  test("roundDown: rounds down to nearest hundredth of a currency unit") {
+    assertEquals(AmendmentData.roundDown(10.995).toDouble, 10.99)
+  }
+
+  test("roundDown: ignores already rounded values") {
+    assertEquals(AmendmentData.roundDown(10.1).toDouble, 10.1)
+  }
+
+  test("roundDown: rounds down if halfway between two values") {
+    assertEquals(AmendmentData.roundDown(10.255).toDouble, 10.25)
   }
 }
