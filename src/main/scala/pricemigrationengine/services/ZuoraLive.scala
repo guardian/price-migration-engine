7aef80eca53e43f1360a66750e1d586be4c0aec1
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-05-13 09:23:21 +0100

    Only generate Zuora access token once

--- src/main/scala/pricemigrationengine/services/ZuoraLive.scala
+++ src/main/scala/pricemigrationengine/services/ZuoraLive.scala
@@ -29,36 +29,49 @@ object ZuoraLive {
           private case class InvoicePreviewRequest(accountId: String, targetDate: LocalDate)
           private implicit val rwInvoicePreviewRequest: ReadWriter[InvoicePreviewRequest] = macroRW
 
-          private lazy val fetchedAccessToken: ZIO[Any, ZuoraFetchFailure, String] =
-            handleRequest[AccessToken](
-              Http(s"${config.zuora.apiHost}/oauth/token")
-                .postForm(
-                  Seq(
-                    "grant_type" -> "client_credentials",
-                    "client_id" -> config.zuora.clientId,
-                    "client_secret" -> config.zuora.clientSecret
-                  )
+          /*
+           * The access token is generated outside the ZIO framework so that it's only fetched once.
+           * There has to be a better way to do this, but don't know what it is at the moment.
+           * Possibly memoize the value at a higher level.
+           * Will return to it.
+           */
+          private lazy val fetchedAccessToken: Either[ZuoraFetchFailure, String] = {
+            val response = Http(s"${config.zuora.apiHost}/oauth/token")
+              .postForm(
+                Seq(
+                  "grant_type" -> "client_credentials",
+                  "client_id" -> config.zuora.clientId,
+                  "client_secret" -> config.zuora.clientSecret
                 )
-            ).map(_.access_token)
-              .tap(_ => logging.info("Fetched Zuora access token"))
+              )
+              .asString
+            val body = response.body
+            if (response.code == 200)
+              Right(read[AccessToken](body).access_token)
+                .orElse(Left(ZuoraFetchFailure(body)))
+            else
+              Left(ZuoraFetchFailure(body))
+          }
 
           private def get[A: Reader](path: String): ZIO[Any, ZuoraFetchFailure, A] =
-            fetchedAccessToken flatMap { accessToken =>
-              handleRequest[A](
+            for {
+              accessToken <- ZIO.fromEither(fetchedAccessToken)
+              a <- handleRequest[A](
                 Http(s"${config.zuora.apiHost}/v1/$path")
                   .header("Authorization", s"Bearer $accessToken")
               )
-            }
+            } yield a
 
           private def post[A: Reader](path: String, body: String): ZIO[Any, ZuoraFetchFailure, A] =
-            fetchedAccessToken flatMap { accessToken =>
-              handleRequest[A](
+            for {
+              accessToken <- ZIO.fromEither(fetchedAccessToken)
+              a <- handleRequest[A](
                 Http(s"${config.zuora.apiHost}/v1/$path")
                   .header("Authorization", s"Bearer $accessToken")
                   .header("Content-Type", "application/json")
                   .postData(body)
               )
-            }
+            } yield a
 
           private def handleRequest[A: Reader](request: HttpRequest): ZIO[Any, ZuoraFetchFailure, A] = {
             val response = request
