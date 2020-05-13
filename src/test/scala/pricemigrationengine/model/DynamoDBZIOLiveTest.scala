937bf08a8166e7f70c9695689e754627aee6f091
Francis Rhys-Jones <francis@rhys-jones.com>
2020-05-13 13:07:33 +0100

    adding test for dynamodb updates

--- src/test/scala/pricemigrationengine/model/DynamoDBZIOLiveTest.scala
+++ src/test/scala/pricemigrationengine/model/DynamoDBZIOLiveTest.scala
@@ -1,8 +1,10 @@
 package pricemigrationengine.model
 
+import java.util
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.model.{AttributeValue, QueryRequest, QueryResult, UpdateItemResult}
-import pricemigrationengine.services.{CohortTable, CohortTableLive, Configuration, ConsoleLogging, DynamoDBDeserialiser, DynamoDBZIO, DynamoDBZIOError, DynamoDBZIOLive}
+import com.amazonaws.services.dynamodbv2.model.{AttributeAction, AttributeValue, AttributeValueUpdate, QueryRequest, QueryResult, UpdateItemRequest, UpdateItemResult}
+import pricemigrationengine.services.{CohortTable, CohortTableLive, Configuration, ConsoleLogging, DynamoDBDeserialiser, DynamoDBSerialiser, DynamoDBUpdateSerialiser, DynamoDBZIO, DynamoDBZIOError, DynamoDBZIOLive}
 import zio.Exit.Success
 import zio.Runtime.default
 import zio.stream.Sink
@@ -22,6 +24,7 @@ class DynamoDBZIOLiveTest extends munit.FunSuite {
 
   test("DynamoDBZIOLive should get all batches of query results and convert the batches to a stream") {
     def item(id: String) = Map("id" -> new AttributeValue(id)).asJava
+
     implicit val itemDeserialiser = new DynamoDBDeserialiser[String] {
       def deserialise(value: java.util.Map[String, AttributeValue]) =
         ZIO.fromOption(value.asScala.get("id").map(_.getS))
@@ -31,7 +34,7 @@ class DynamoDBZIOLiveTest extends munit.FunSuite {
     val queryRequest = new QueryRequest()
     val responseMap = Map(
       queryRequest.clone() -> new QueryResult()
-        .withItems(item("id-1"),item("id-2"))
+        .withItems(item("id-1"), item("id-2"))
         .withLastEvaluatedKey(item("id-2")),
       queryRequest.clone().withExclusiveStartKey(item("id-2")) -> new QueryResult()
         .withItems(item("id-3"))
@@ -57,5 +60,51 @@ class DynamoDBZIOLiveTest extends munit.FunSuite {
       case failure =>
         fail(s"Query returned failure $failure")
     }
+  }
+
+  test("DynamoDBZIOLive serialize key and values and update in dynamodb") {
+    implicit val keySerialiser = new DynamoDBSerialiser[String] {
+      override def serialise(key: String): util.Map[String, AttributeValue] =
+        Map("key" -> new AttributeValue().withS(key)).asJava
+    }
+
+    implicit val updateSerialiser = new DynamoDBUpdateSerialiser[String] {
+      override def serialise(value: String): util.Map[String, AttributeValueUpdate] =
+        Map("value" -> new AttributeValueUpdate(new AttributeValue().withS(value), AttributeAction.PUT)).asJava
+    }
+
+    var receivedUpdateItemRequest: Option[UpdateItemRequest] = None
+
+    val stubDynamoDBClient = ZLayer.succeed[AmazonDynamoDB](
+      new AmazonDynamoDBSubBase {
+        override def updateItem(updateItemRequest: UpdateItemRequest): UpdateItemResult = {
+          receivedUpdateItemRequest = Some(updateItemRequest)
+          new UpdateItemResult()
+        }
+      }
+    )
+
+    assertEquals(
+      default.unsafeRunSync(
+        DynamoDBZIO
+          .update("a-table", "key", "value")
+          .provideLayer((stubConfiguration ++ stubDynamoDBClient ++ stubLogging) >>> DynamoDBZIOLive.impl)
+      ),
+      Success(())
+    )
+
+    assertEquals(
+      receivedUpdateItemRequest,
+      Some(
+        new UpdateItemRequest()
+          .withTableName("a-table")
+          .withKey(
+            Map("key" -> new AttributeValue().withS("key")).asJava
+          )
+          .withAttributeUpdates(
+            Map("value" -> new AttributeValueUpdate(new AttributeValue().withS("value"), AttributeAction.PUT)).asJava
+          )
+      )
+    )
   }
 }
