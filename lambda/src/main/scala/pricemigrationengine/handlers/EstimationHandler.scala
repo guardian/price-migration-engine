fc1107e4d8a721c5b53fef453c8d4ea7813c1ab8
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-06-04 16:49:05 +0100

    Update status on estimation failure
    Instead of stopping.
    This allows lambda to continue on its own.

--- lambda/src/main/scala/pricemigrationengine/handlers/EstimationHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/EstimationHandler.scala
@@ -3,7 +3,7 @@ package pricemigrationengine.handlers
 import java.time.{Instant, LocalDate}
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import pricemigrationengine.model.CohortTableFilter.{EstimationComplete, ReadyForEstimation}
+import pricemigrationengine.model.CohortTableFilter.{EstimationComplete, EstimationFailed, ReadyForEstimation}
 import pricemigrationengine.model._
 import pricemigrationengine.services._
 import zio.console.Console
@@ -16,12 +16,24 @@ object EstimationHandler extends zio.App with RequestHandler[Unit, Unit] {
     for {
       newProductPricing <- Zuora.fetchProductCatalogue.map(ZuoraProductCatalogue.productPricingMap)
       cohortItems <- CohortTable.fetch(ReadyForEstimation, None)
-      _ <- cohortItems.foreach(writeEstimation(newProductPricing))
+      _ <- cohortItems.foreach(estimate(newProductPricing))
     } yield ()
 
-  private def writeEstimation(
+  private def estimate(
       newProductPricing: ZuoraPricingData
   )(item: CohortItem): ZIO[Logging with AmendmentConfiguration with CohortTable with Zuora with Random, Failure, Unit] =
+    doEstimation(newProductPricing, item).foldM(
+      failure = {
+        case _: AmendmentDataFailure => CohortTable.update(CohortItem(item.subscriptionName, EstimationFailed))
+        case e                       => ZIO.fail(e)
+      },
+      success = CohortTable.update
+    )
+
+  private def doEstimation(
+      newProductPricing: ZuoraPricingData,
+      item: CohortItem
+  ): ZIO[Logging with AmendmentConfiguration with CohortTable with Zuora with Random, Failure, CohortItem] =
     for {
       subscription <- Zuora.fetchSubscription(item.subscriptionName)
       invoicePreview <- Zuora.fetchInvoicePreview(subscription.accountId)
@@ -33,20 +45,16 @@ object EstimationHandler extends zio.App with RequestHandler[Unit, Unit] {
             Logging.error(s"Failed to estimate amendment data for subscription ${subscription.subscriptionNumber}: $e"),
           result => Logging.info(s"Estimated result: $result")
         )
-      _ <- CohortTable
-        .update(
-          CohortItem(
-            result.subscriptionName,
-            processingStage = EstimationComplete,
-            oldPrice = Some(result.oldPrice),
-            estimatedNewPrice = Some(result.estimatedNewPrice),
-            currency = Some(result.currency),
-            startDate = Some(result.startDate),
-            billingPeriod = Some(result.billingPeriod),
-            whenEstimationDone = Some(Instant.now())
-          )
-        )
-    } yield ()
+    } yield CohortItem(
+      result.subscriptionName,
+      processingStage = EstimationComplete,
+      oldPrice = Some(result.oldPrice),
+      estimatedNewPrice = Some(result.estimatedNewPrice),
+      currency = Some(result.currency),
+      startDate = Some(result.startDate),
+      billingPeriod = Some(result.billingPeriod),
+      whenEstimationDone = Some(Instant.now())
+    )
 
   /*
    * Earliest start date spread out over 3 months.
