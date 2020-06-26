63f4ec5569cbe37487a1f341db20bdd93888cbf2
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-06-26 15:57:32 +0100

    Bump dependencies (#103)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val zioVersion = "1.0.0-RC20"
-  private val awsSdkVersion = "1.11.792"
+  private val awsSdkVersion = "1.11.811"
 
   lazy val awsDynamoDb = "com.amazonaws" % "aws-java-sdk-dynamodb" % awsSdkVersion
   lazy val awsS3 = "com.amazonaws" % "aws-java-sdk-s3" % awsSdkVersion
@@ -12,6 +12,6 @@ object Dependencies {
   lazy val upickle = "com.lihaoyi" %% "upickle" % "1.1.0"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
   lazy val http = "org.scalaj" %% "scalaj-http" % "2.4.2"
-  lazy val munit = "org.scalameta" %% "munit" % "0.7.8"
+  lazy val munit = "org.scalameta" %% "munit" % "0.7.9"
   lazy val commonsCsv = "org.apache.commons" % "commons-csv" % "1.8"
 }
