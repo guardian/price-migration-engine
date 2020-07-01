864b8d36112d3c84737c133f365b72bdf2220561
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-07-01 13:16:26 +0100

    Refactor AmendmentHandler (#110)

--- lambda/src/main/scala/pricemigrationengine/services/Logging.scala
+++ lambda/src/main/scala/pricemigrationengine/services/Logging.scala
@@ -1,5 +1,6 @@
 package pricemigrationengine.services
 
+import pricemigrationengine.model.{CohortItem, Failure}
 import zio.{UIO, ZIO}
 
 object Logging {
@@ -7,6 +8,12 @@ object Logging {
   trait Service {
     def info(s: String): UIO[Unit]
     def error(s: String): UIO[Unit]
+
+    def logSuccess[A](cohortItem: CohortItem)(result: A): UIO[Unit] =
+      info(s"Subscription ${cohortItem.subscriptionName} succeeded: $result")
+
+    def logFailure(cohortItem: CohortItem)(failure: Failure): UIO[Unit] =
+      error(s"Subscription ${cohortItem.subscriptionName} failed: $failure")
   }
 
   def info(s: String): ZIO[Logging, Nothing, Unit] =
@@ -14,4 +21,10 @@ object Logging {
 
   def error(s: String): ZIO[Logging, Nothing, Unit] =
     ZIO.accessM(_.get.error(s))
+
+  def logSuccess[A](cohortItem: CohortItem)(result: A): ZIO[Logging, Nothing, Unit] =
+    ZIO.accessM(_.get.logSuccess(cohortItem)(result))
+
+  def logFailure(cohortItem: CohortItem)(failure: Failure): ZIO[Logging, Nothing, Unit] =
+    ZIO.accessM(_.get.logFailure(cohortItem)(failure))
 }
