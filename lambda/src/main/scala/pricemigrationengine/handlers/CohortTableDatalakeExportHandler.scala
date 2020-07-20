fe367e18d544bc45b19496e92ec59d6c2d0c22be
Francis Rhys-Jones <francis@rhys-jones.com>
2020-07-20 17:26:01 +0100

    writing all cohort item fields in datalake export

--- lambda/src/main/scala/pricemigrationengine/handlers/CohortTableDatalakeExportHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/CohortTableDatalakeExportHandler.scala
@@ -18,44 +18,46 @@ object CohortTableDatalakeExportHandler extends CohortHandler {
   private val TempFilePath = "/tmp/"
 
   def main(
-    cohortSpec: CohortSpec
+      cohortSpec: CohortSpec
   ): ZIO[Logging with CohortTable with S3 with StageConfiguration with Clock, Failure, HandlerOutput] =
     for {
       config <- StageConfiguration.stageConfig
       records <- CohortTable.fetchAll()
-      today <- Time.today
       s3Location = S3Location(
         s"price-migration-engine-${config.stage.toLowerCase}",
-        s"${cohortSpec.cohortName}.csv"
+        s"${cohortSpec.tableName}.csv"
       )
-      recordCount <- writeCsvToS3(records, s3Location)
-      _ <- Logging
-        .info(s"Wrote $recordCount CohortItems to s3: $s3Location")
+      _ <- writeCsvToS3(records, s3Location, cohortSpec)
     } yield HandlerOutput(isComplete = true)
 
   def writeCsvToS3(
-    cohortItems: ZStream[Any, CohortFetchFailure, CohortItem],
-    s3Location: S3Location
-  ): ZIO[S3 with Logging, Failure, Long] =
-    localTempFile(TempFilePath + "")
-      .use( filePath =>
-        for {
-          recordsWritten <- openOutputStream(filePath).use { tempFileOutputStream =>
-            writeCsvToStream(cohortItems, tempFileOutputStream)
-          }
+      cohortItems: ZStream[Any, CohortFetchFailure, CohortItem],
+      s3Location: S3Location,
+      cohortSpec: CohortSpec
+  ): ZIO[S3 with Logging, Failure, Unit] =
+    localTempFile(TempFilePath + "").use { filePath =>
+      for {
+        recordsWritten <- openOutputStream(filePath).use { tempFileOutputStream =>
+          writeCsvToStream(cohortItems, tempFileOutputStream, cohortSpec)
+        }
 
-          putResult <- S3.putObject(s3Location, filePath.toFile)
+        putResult <-
+          S3.putObject(s3Location, filePath.toFile)
             .mapError { failure =>
               CohortTableDatalakeExportFailure(s"Failed to write CohortItems to s3: ${failure.reason}")
             }
 
-          _ <- Logging.info(s"Successfully wrote cohort table to $s3Location: $putResult")
-        } yield recordsWritten
-      )
+        _ <- Logging.info(
+          s"Successfully wrote cohort table ${cohortSpec.tmpTableName} containing $recordsWritten items " +
+            s"to $s3Location: $putResult"
+        )
+      } yield recordsWritten
+    }
 
   def localTempFile(path: String): ZManaged[Any, CohortTableDatalakeExportFailure, Path] =
     ZManaged.make(
-      ZIO.effect(Files.createTempFile(new File("/tmp/").toPath, "CohortTableExport", "csv"))
+      ZIO
+        .effect(Files.createTempFile(new File("/tmp/").toPath, "CohortTableExport", "csv"))
         .mapError { throwable =>
           CohortTableDatalakeExportFailure(s"Failed to create temp file $path: ${throwable.getMessage}")
         }
@@ -63,40 +65,90 @@ object CohortTableDatalakeExportHandler extends CohortHandler {
 
   def openOutputStream(path: Path): ZManaged[Any, CohortTableDatalakeExportFailure, OutputStream] =
     ZManaged.make(
-      ZIO.effect(Files.newOutputStream(path))
+      ZIO
+        .effect(Files.newOutputStream(path))
         .mapError { throwable =>
           CohortTableDatalakeExportFailure(s"Failed to write to temp files $path: ${throwable.getMessage}")
         }
-      )(stream => ZIO.effectTotal(stream.close()))
+    )(stream => ZIO.effectTotal(stream.close()))
 
   def writeCsvToStream(
-    cohortItems: ZStream[Any, Failure, CohortItem],
-    outputStream: OutputStream
+      cohortItems: ZStream[Any, Failure, CohortItem],
+      outputStream: OutputStream,
+      cohortSpec: CohortSpec
   ): IO[Failure, Long] = {
-    val managedCsvPrinter = ZManaged.makeEffect(
-      new CSVPrinter(
-        new OutputStreamWriter(outputStream, StandardCharsets.UTF_8.name()),
-        csvFormat
+    managedCSVPrinter(
+      outputStream,
+      List(
+        "cohortName",
+        "cohortTable",
+        "subscriptionName",
+        "processingStage",
+        "startDate",
+        "currency",
+        "oldPrice",
+        "estimatedNewPrice",
+        "billingPeriod",
+        "whenEstimationDone",
+        "salesforcePriceRiseId",
+        "whenSfShowEstimate",
+        "newPrice",
+        "newSubscriptionId",
+        "whenAmendmentDone",
+        "whenNotificationSent",
+        "whenNotificationSentWrittenToSalesforce",
+        "whenAmendmentWrittenToSalesforce"
       )
-    )(printer => printer.close(true)).mapError { ex =>
-      CohortTableDatalakeExportFailure(s"Failed to write CohortItems as CSV to s3: ${ex.getMessage}")
-    }
-
-    managedCsvPrinter.use { printer =>
-     cohortItems
-       .mapM { cohortItem =>
-          ZIO
-            .effect(printer.printRecord("abc"))
-            .mapError { ex =>
-              CohortTableDatalakeExportFailure(s"Failed to write CohortItem as CSV to s3: ${ex.getMessage}")
-            }
-        }
-        .runCount
+    ).use { printer =>
+      cohortItems.mapM { cohortItem =>
+        ZIO
+          .effect(
+            printer.printRecord(
+              cohortSpec.cohortName,
+              cohortSpec.tmpTableName,
+              cohortItem.subscriptionName,
+              cohortItem.processingStage.value,
+              cohortItem.startDate.getOrElse(""),
+              cohortItem.currency.getOrElse(""),
+              cohortItem.oldPrice.getOrElse(""),
+              cohortItem.estimatedNewPrice.getOrElse(""),
+              cohortItem.billingPeriod.getOrElse(""),
+              cohortItem.whenEstimationDone.getOrElse(""),
+              cohortItem.salesforcePriceRiseId.getOrElse(""),
+              cohortItem.whenSfShowEstimate.getOrElse(""),
+              cohortItem.newPrice.getOrElse(""),
+              cohortItem.newSubscriptionId.getOrElse(""),
+              cohortItem.whenAmendmentDone.getOrElse(""),
+              cohortItem.whenNotificationSent.getOrElse(""),
+              cohortItem.whenNotificationSentWrittenToSalesforce.getOrElse(""),
+              cohortItem.whenAmendmentWrittenToSalesforce.getOrElse("")
+            )
+          )
+          .mapError { ex =>
+            CohortTableDatalakeExportFailure(s"Failed to write CohortItem as CSV to s3: ${ex.getMessage}")
+          }
+      }.runCount
     }
   }
 
+  private def managedCSVPrinter(
+      outputStream: OutputStream,
+      headers: List[String]
+  ): ZManaged[Any, CohortTableDatalakeExportFailure, CSVPrinter] = {
+    ZManaged
+      .makeEffect(
+        new CSVPrinter(
+          new OutputStreamWriter(outputStream, StandardCharsets.UTF_8.name()),
+          csvFormat.withHeader(headers: _*)
+        )
+      )(printer => printer.close(true))
+      .mapError { ex =>
+        CohortTableDatalakeExportFailure(s"Failed to write CohortItems as CSV to s3: ${ex.getMessage}")
+      }
+  }
+
   private def env(
-    cohortSpec: CohortSpec
+      cohortSpec: CohortSpec
   ): ZLayer[Logging, Failure, CohortTable with S3 with Logging with StageConfiguration] =
     (LiveLayer.cohortTable(cohortSpec.tableName) and LiveLayer.s3 and LiveLayer.logging and LiveLayer.stageConfig)
       .tapError(e => Logging.error(s"Failed to create service environment: $e"))
