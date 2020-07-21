4493266595c1bf1255f4e205380983a44cfa8452
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-07-21 14:03:56 +0100

    Configure lambda that kicks off state machines (#131)

--- lambda/src/main/scala/pricemigrationengine/services/CohortStateMachineLive.scala
+++ lambda/src/main/scala/pricemigrationengine/services/CohortStateMachineLive.scala
@@ -6,23 +6,28 @@ import com.amazonaws.regions.Regions.EU_WEST_1
 import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder
 import com.amazonaws.services.stepfunctions.model.{StartExecutionRequest, StartExecutionResult}
 import pricemigrationengine.model._
-import upickle.default.write
+import upickle.default.{ReadWriter, macroRW, write}
 import zio.blocking.Blocking
 import zio.{IO, ZLayer}
 
 object CohortStateMachineLive {
 
+  private case class StateMachineInput(cohortSpec: CohortSpec)
+  private implicit val rw: ReadWriter[StateMachineInput] = macroRW
+
   val impl
-    : ZLayer[CohortStateMachineConfiguration with Logging with Blocking, ConfigurationFailure, CohortStateMachine] = {
+      : ZLayer[CohortStateMachineConfiguration with Logging with Blocking, ConfigurationFailure, CohortStateMachine] = {
 
     val stateMachine = AWSStepFunctionsClientBuilder.standard.withRegion(EU_WEST_1).build
 
-    ZLayer.fromServicesM[CohortStateMachineConfiguration.Service,
-                         Logging.Service,
-                         Blocking.Service,
-                         Any,
-                         ConfigurationFailure,
-                         CohortStateMachine.Service] { (configuration, logging, blocking) =>
+    ZLayer.fromServicesM[
+      CohortStateMachineConfiguration.Service,
+      Logging.Service,
+      Blocking.Service,
+      Any,
+      ConfigurationFailure,
+      CohortStateMachine.Service
+    ] { (configuration, logging, blocking) =>
       configuration.config map { config =>
         new CohortStateMachine.Service {
           def startExecution(date: LocalDate)(spec: CohortSpec): IO[CohortStateMachineFailure, StartExecutionResult] =
@@ -32,7 +37,9 @@ object CohortStateMachineLive {
                   new StartExecutionRequest()
                     .withStateMachineArn(config.stateMachineArn)
                     .withName(s"${spec.cohortName}-$date")
-                    .withInput(write(spec))))
+                    .withInput(write(StateMachineInput(spec)))
+                )
+              )
               .mapError(e => CohortStateMachineFailure(s"Failed to start execution: $e"))
               .tap(result => logging.info(s"Started execution: $result"))
         }
