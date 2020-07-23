eea742ac1158a87d730ac9b18991eb892c974060
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-07-23 11:00:27 +0100

    Modify logic of recipient's title and first name (#136)
    When there's no first name, we use the title as the first name
    and leave the title field empty.

--- lambda/src/main/scala/pricemigrationengine/handlers/NotificationHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/NotificationHandler.scala
@@ -74,7 +74,9 @@ object NotificationHandler extends CohortHandler {
     for {
       _ <- Logging.info(s"Processing subscription: ${cohortItem.subscriptionName}")
       contact <- SalesforceClient.getContact(sfSubscription.Buyer__c)
-      firstName <- requiredField(contact.FirstName, "Contact.FirstName")
+      firstName <- requiredField(contact.FirstName, "Contact.FirstName").orElse(
+        requiredField(contact.Salutation, "Contact.Salutation")
+      )
       lastName <- requiredField(contact.LastName, "Contact.LastName")
       targetAddress <- requiredField(contact.OtherAddress, "Contact.OtherAddress").orElse(
         requiredField(contact.MailingAddress, "Contact.MailingAddress")
@@ -97,7 +99,9 @@ object NotificationHandler extends CohortHandler {
             Address = contact.Email,
             ContactAttributes = EmailPayloadContactAttributes(
               SubscriberAttributes = EmailPayloadSubscriberAttributes(
-                title = contact.Salutation,
+                title = contact.FirstName flatMap (_ =>
+                  contact.Salutation
+                ), // if no first name, we use salutation as first name and leave this field empty
                 first_name = firstName,
                 last_name = lastName,
                 billing_address_1 = street,
