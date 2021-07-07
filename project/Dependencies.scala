da1c2bb09666dc4029e6e7cb7011c409affb9e13
Scala Steward <me@scala-steward.org>
2021-07-07 09:53:55 +0200

    Update zio, zio-streams, zio-test, ... to 1.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val zioVersion = "1.0.7"
+  private val zioVersion = "1.0.9"
   private val awsSdkVersion = "2.16.96"
 
   lazy val awsDynamoDb = "software.amazon.awssdk" % "dynamodb" % awsSdkVersion
