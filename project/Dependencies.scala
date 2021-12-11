57abba0238606b704a9bc6703491ba34590c8f12
Scala Steward <me@scala-steward.org>
2021-12-11 10:52:36 +0100

    Update upickle to 1.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   lazy val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
   lazy val zioTest = "dev.zio" %% "zio-test" % zioVersion
   lazy val zioTestSbt = "dev.zio" %% "zio-test-sbt" % zioVersion
-  lazy val upickle = "com.lihaoyi" %% "upickle" % "1.4.0"
+  lazy val upickle = "com.lihaoyi" %% "upickle" % "1.4.3"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
   lazy val http = "org.scalaj" %% "scalaj-http" % "2.4.2"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
