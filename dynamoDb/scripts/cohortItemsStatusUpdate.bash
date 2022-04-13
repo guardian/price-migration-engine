7789b5b87ff42a9077c5a0843cf4692d4915b105
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-04-13 10:41:34 +0100

    Generalise update script for any cohort

--- dynamoDb/scripts/cohortItemsStatusUpdate.bash
+++ dynamoDb/scripts/cohortItemsStatusUpdate.bash
@@ -4,19 +4,21 @@
 # Updates the cohort table processingStage attribute,
 # for a given list of subscriptions.
 #
-# Arg 1: path to a file containing line-separated subscription numbers.
+# Arg 1: Cohort name
+# Arg 2: path to a file containing line-separated subscription numbers
 # =====================================================================
 
-# Arg 1: Deployment stage: DEV, CODE or PROD
-# Arg 2: New processingStage
-# Arg 3: Subscription number
+# Arg 1: Cohort name
+# Arg 2: Deployment stage: DEV, CODE or PROD
+# Arg 3: New processingStage
+# Arg 4: Subscription number
 function update() {
   aws dynamodb update-item --region eu-west-1 --profile membership \
-    --table-name "PriceMigrationEngine$1" \
-    --key "{\"subscriptionNumber\": {\"S\": \"$3\"}}" \
+    --table-name "PriceMigration-$2-$1" \
+    --key "{\"subscriptionNumber\": {\"S\": \"$4\"}}" \
     --update-expression "SET #P = :p" \
     --expression-attribute-names "{\"#P\": \"processingStage\"}" \
-    --expression-attribute-values "{\":p\": {\"S\": \"$2\"}}" \
+    --expression-attribute-values "{\":p\": {\"S\": \"$3\"}}" \
     --return-values UPDATED_OLD
 }
 
@@ -27,5 +29,5 @@ status=EstimationComplete
 while read -r line; do
   sub=$(echo "$line" | tr -cd "[:print:]")
   echo "Updating $stage $sub to status $status";
-  update "$stage" "$status" "$sub";
-done <"$1"
+  update "$1" "$stage" "$status" "$sub";
+done <"$2"
