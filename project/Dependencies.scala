d3f5dc01171c215aa1ab8e9828e302c2f0cbb373
Scala Steward <scala-steward@users.noreply.github.com>
2022-06-10 13:28:07 +0000

    Update dynamodb, s3, sfn, sqs to 2.17.208

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val zioVersion = "2.0.0-RC6"
-  private val awsSdkVersion = "2.17.193"
+  private val awsSdkVersion = "2.17.208"
 
   lazy val awsDynamoDb = "software.amazon.awssdk" % "dynamodb" % awsSdkVersion
   lazy val awsS3 = "software.amazon.awssdk" % "s3" % awsSdkVersion
