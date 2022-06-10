12af65db694a3fc18fe51a32abeb119c619c894e
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-06-10 10:31:00 +0100

    Don't add build number to ConsoleLogging
    As it will always be 'unknown' so redundant.

--- lambda/src/main/scala/pricemigrationengine/services/ConsoleLogging.scala
+++ lambda/src/main/scala/pricemigrationengine/services/ConsoleLogging.scala
@@ -1,17 +1,12 @@
 package pricemigrationengine.services
 
-import build.BuildInfo.buildNumber
 import zio.{Console, UIO, ULayer, ZLayer}
 
 object ConsoleLogging {
 
   def impl(cohortName: String): ULayer[Logging] =
     ZLayer.succeed(new Logging {
-
-      private val logPrefix = s"CohortName: $cohortName, BuildNumber: $buildNumber"
-
-      override def info(s: String): UIO[Unit] = Console.printLine(s"$logPrefix, INFO: $s").orDie
-
-      override def error(s: String): UIO[Unit] = Console.printLine(s"$logPrefix, ERROR: $s").orDie
+      override def info(s: String): UIO[Unit] = Console.printLine(s"cohortName: $cohortName, INFO: $s").orDie
+      override def error(s: String): UIO[Unit] = Console.printLine(s"cohortName: $cohortName, ERROR: $s").orDie
     })
 }
