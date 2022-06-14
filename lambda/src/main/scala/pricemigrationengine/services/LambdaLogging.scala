8e531d87e5b4e06f1b33f7de4ee2f1c5ee45d028
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-06-14 08:55:11 +0100

    Add build number to logs (#594)

--- lambda/src/main/scala/pricemigrationengine/services/LambdaLogging.scala
+++ lambda/src/main/scala/pricemigrationengine/services/LambdaLogging.scala
@@ -1,16 +1,19 @@
 package pricemigrationengine.services
 
+import build.BuildInfo.buildNumber
 import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
-import zio.{UIO, ULayer, ZIO, ZLayer}
 import upickle.default.{ReadWriter, macroRW, write}
+import zio.{UIO, ULayer, ZIO, ZLayer}
 
 object LambdaLogging {
   private case class InfoMessage(
+      Build: String,
       CohortName: String,
       INFO: String
   )
 
   private case class ErrorMessage(
+      Build: String,
       CohortName: String,
       ERROR: String
   )
@@ -23,9 +26,9 @@ object LambdaLogging {
       new Logging {
         val logger: LambdaLogger = context.getLogger
         override def info(s: String): UIO[Unit] =
-          ZIO.succeed(logger.log(write(InfoMessage(cohortName, s))))
+          ZIO.succeed(logger.log(write(InfoMessage(buildNumber, cohortName, s))))
         override def error(s: String): UIO[Unit] =
-          ZIO.succeed(logger.log(write(ErrorMessage(cohortName, s))))
+          ZIO.succeed(logger.log(write(ErrorMessage(buildNumber, cohortName, s))))
       }
     )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,4 @@
 addSbtPlugin("com.gu" % "sbt-riffraff-artifact" % "1.1.18")
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "1.2.0")
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
