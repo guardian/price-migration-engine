be8cb7e5f55bbbb1e177b00bd68db916379adb7d
Pascal Honoré <pascal@alseyn.net>
2023-04-28 09:34:36 +0100

    ZIO type for reading environment variable

--- lambda/src/main/scala/pricemigrationengine/services/EngineSecrets.scala
+++ lambda/src/main/scala/pricemigrationengine/services/EngineSecrets.scala
@@ -2,12 +2,11 @@ package pricemigrationengine.services
 
 import pricemigrationengine.model._
 import zio.{Layer, ZIO, ZLayer}
+import zio._
 import software.amazon.awssdk.regions
 import software.amazon.awssdk.services.secretsmanager._
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest
 import upickle.default._
-
-import java.lang.System.getenv
 
 case class EngineSecrets(
     zuoraApiHost: String,
@@ -29,26 +28,29 @@ object EngineSecrets {
 
   private lazy val secretsClient = SecretsManagerClient.create()
 
-  private lazy val secretId: String = s"price-migration-engine-lambda-${getenv("stage")}"
+  private def getSecretId: ZIO[Any, ConfigFailure, String] =
+    for {
+      stageOpt <- System.env("stage").mapError { ex => ConfigFailure(s"Failure to retrieve stage: ${ex.getMessage}") }
+      stage <- ZIO.fromOption(stageOpt).mapError { _ => ConfigFailure(s"Failure to retrieve stage") }
+    } yield s"price-migration-engine-lambda-${stage}"
 
-  def getSecretString: ZIO[Any, ConfigFailure, String] = {
-    ZIO
+  private def getSecretString: ZIO[Any, ConfigFailure, String] = for {
+    secretId <- getSecretId
+    secret <- ZIO
       .attempt(
         secretsClient.getSecretValue(GetSecretValueRequest.builder().secretId(secretId).build()).secretString()
       )
       .mapError { ex =>
         ConfigFailure(s"Failure to retrieve secrets string: ${ex.getMessage}")
       }
-  }
+  } yield secret
 
-  def getSecrets: ZIO[Any, ConfigFailure, EngineSecrets] = {
-    for {
-      secretJsonString <- getSecretString
-      secrets <- ZIO
-        .attempt(read[EngineSecrets](secretJsonString))
-        .mapError { ex =>
-          ConfigFailure(s"Failure to parse secrets string: ${ex.getMessage}")
-        }
-    } yield secrets
-  }
+  def getSecrets: ZIO[Any, ConfigFailure, EngineSecrets] = for {
+    secretJsonString <- getSecretString
+    secrets <- ZIO
+      .attempt(read[EngineSecrets](secretJsonString))
+      .mapError { ex =>
+        ConfigFailure(s"Failure to parse secrets string: ${ex.getMessage}")
+      }
+  } yield secrets
 }
