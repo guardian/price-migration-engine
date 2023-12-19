9c439dcf61d297fd457f31f987d36960ea457d0b
Pascal Honoré <pascal@alseyn.net>
2023-12-19 22:49:50 +0000

    implement priceData

--- lambda/src/main/scala/pricemigrationengine/migrations/Newspaper2024Migration.scala
+++ lambda/src/main/scala/pricemigrationengine/migrations/Newspaper2024Migration.scala
@@ -1,9 +1,30 @@
 package pricemigrationengine.migrations
 import pricemigrationengine.model._
+import pricemigrationengine.services.Zuora
 
 import java.time.LocalDate
 
 object Newspaper2024Migration {
+
+  /*
+    Correspondence between product names in Salesforce versus Zuora
+
+    -----------------------------------------------------------------
+    Product name in the Salesforce export | Product name in Zuora
+    -----------------------------------------------------------------
+    Newspaper - Home Delivery             | Newspaper Delivery
+    Newspaper - Subscription Card         | Newspaper Digital Voucher
+    Newspaper - Voucher Book              | Newspaper Voucher
+    -----------------------------------------------------------------
+   */
+
+  case class RatePlanDetails(
+      ratePlan: ZuoraRatePlan,
+      ratePlanName: String,
+      billingPeriod: BillingPeriod,
+      currency: String,
+      currentPrice: BigDecimal
+  )
 
   private val newspaperHomeDeliveryPricesMonthly: Map[String, BigDecimal] = Map(
     "Everyday" -> BigDecimal(78.99),
@@ -101,22 +122,136 @@ object Newspaper2024Migration {
     "Weekend+" -> BigDecimal(419.88),
   )
 
-  def priceLookup(product: String, billingPeriod: BillingPeriod, paymentFrequency: String): Option[BigDecimal] = {
+  def priceLookup(product: String, billingPeriod: BillingPeriod, ratePlanName: String): Option[BigDecimal] = {
     val empty: Map[String, BigDecimal] = Map()
     val priceMap = (product, billingPeriod) match {
-      case ("Newspaper - Home Delivery", Monthly)        => newspaperHomeDeliveryPricesMonthly
-      case ("Newspaper - Home Delivery", Quarterly)      => newspaperHomeDeliveryPricesQuarterly
-      case ("Newspaper - Subscription Card", Monthly)    => newspaperSubscriptionCardMonthly
-      case ("Newspaper - Subscription Card", Quarterly)  => newspaperSubscriptionCardQuarterly
-      case ("Newspaper - Subscription Card", SemiAnnual) => newspaperSubscriptionCardSemiAnnual
-      case ("Newspaper - Subscription Card", Annual)     => newspaperSubscriptionCardAnnual
-      case ("Newspaper - Voucher Book", Monthly)         => newspaperVoucherBookMonthly
-      case ("Newspaper - Voucher Book", Quarterly)       => newspaperVoucherBookQuarterly
-      case ("Newspaper - Voucher Book", SemiAnnual)      => newspaperVoucherBookSemiAnnual
-      case ("Newspaper - Voucher Book", Annual)          => newspaperVoucherBookAnnual
-      case _                                             => empty
+      case ("Newspaper Delivery", Monthly)           => newspaperHomeDeliveryPricesMonthly
+      case ("Newspaper Delivery", Quarterly)         => newspaperHomeDeliveryPricesQuarterly
+      case ("Newspaper Digital Voucher", Monthly)    => newspaperSubscriptionCardMonthly
+      case ("Newspaper Digital Voucher", Quarterly)  => newspaperSubscriptionCardQuarterly
+      case ("Newspaper Digital Voucher", SemiAnnual) => newspaperSubscriptionCardSemiAnnual
+      case ("Newspaper Digital Voucher", Annual)     => newspaperSubscriptionCardAnnual
+      case ("Newspaper Voucher", Monthly)            => newspaperVoucherBookMonthly
+      case ("Newspaper Voucher", Quarterly)          => newspaperVoucherBookQuarterly
+      case ("Newspaper Voucher", SemiAnnual)         => newspaperVoucherBookSemiAnnual
+      case ("Newspaper Voucher", Annual)             => newspaperVoucherBookAnnual
+      case _                                         => empty
     }
-    priceMap.get(paymentFrequency)
+    priceMap.get(ratePlanName)
   }
 
+  def subscriptionToMigrationProductName(subscription: ZuoraSubscription): Either[String, String] = {
+    val migrationProductNames = List("Newspaper Delivery", "Newspaper Digital Voucher", "Newspaper Voucher")
+    val names = subscription.ratePlans
+      .filter(ratePlan => ratePlan.lastChangeType == None || ratePlan.lastChangeType == Some("Add"))
+      .map(ratePlan => ratePlan.productName)
+      .filter(name => migrationProductNames.contains(name))
+      .distinct
+    names match {
+      case Nil =>
+        Left(
+          s"[error: d5fb6922] could not determine migration product name for subscription ${subscription.subscriptionNumber}; no name to choose from"
+        )
+      case name :: Nil => Right(name)
+      case _ =>
+        Left(
+          s"[error: d3ecd18d] could not determine migration product name for subscription ${subscription.subscriptionNumber}; more than one name to choose from"
+        )
+    }
+  }
+
+  def ratePlanToBillingPeriod(ratePlan: ZuoraRatePlan): Option[BillingPeriod] = {
+    for {
+      ratePlanCharge <- ratePlan.ratePlanCharges.headOption
+      billingPeriod <- ratePlanCharge.billingPeriod
+    } yield BillingPeriod.fromString(billingPeriod)
+  }
+
+  def ratePlanToCurrency(ratePlan: ZuoraRatePlan): Option[String] = {
+    for {
+      ratePlanCharge <- ratePlan.ratePlanCharges.headOption
+    } yield ratePlanCharge.currency
+  }
+
+  def subscriptionToRatePlanDetails(
+      subscription: ZuoraSubscription,
+      productName: String
+  ): Either[String, RatePlanDetails] = {
+    val ratePlans = {
+      subscription.ratePlans
+        .filter(ratePlan => ratePlan.productName == productName)
+        .filter(ratePlan => ratePlan.lastChangeType == None || ratePlan.lastChangeType == Some("Add"))
+        .distinct
+    }
+    ratePlans match {
+      case Nil =>
+        Left(
+          s"[error 93a21a48] Subscription ${subscription.subscriptionNumber} was found to have zero newsPaperDeliveryRatePlans making determination of rate plan name impossible"
+        )
+      case ratePlan :: Nil => {
+        (for {
+          billingPeriod <- ratePlanToBillingPeriod(ratePlan)
+          currency <- ratePlanToCurrency(ratePlan)
+          currentPrice = ratePlan.ratePlanCharges.foldLeft(BigDecimal(0))(
+            (price: BigDecimal, ratePlanCharge: ZuoraRatePlanCharge) =>
+              price + ratePlanCharge.price.getOrElse(BigDecimal(0))
+          )
+        } yield RatePlanDetails(
+          ratePlan,
+          ratePlan.ratePlanName,
+          billingPeriod,
+          currency,
+          currentPrice
+        )) match {
+          case Some(data) => Right(data)
+          case _ => Left(s"[error: 0e218c37] Could not determine billing period for subscription ${subscription}")
+        }
+      }
+      case _ =>
+        Left(
+          s"[error 93a21a48] Subscription ${subscription.subscriptionNumber} was found to have more than one newsPaperDeliveryRatePlans making determination of rate plan name impossible"
+        )
+    }
+  }
+
+  def subscriptionToNewPrice(subscription: ZuoraSubscription): Option[BigDecimal] = {
+    for {
+      productName <- subscriptionToMigrationProductName(subscription).toOption
+      ratePlanDetails <- subscriptionToRatePlanDetails(subscription, productName).toOption
+      price <- priceLookup(productName, ratePlanDetails.billingPeriod, ratePlanDetails.ratePlanName)
+    } yield price
+  }
+
+  def priceData(
+      subscription: ZuoraSubscription,
+  ): Either[AmendmentDataFailure, PriceData] = {
+
+    // PriceData(currency: Currency, oldPrice: BigDecimal, newPrice: BigDecimal, billingPeriod: String)
+
+    def transform1[T](option: Option[T]): Either[AmendmentDataFailure, T] = {
+      option match {
+        case None                 => Left(AmendmentDataFailure("error"))
+        case Some(ratePlanCharge) => Right(ratePlanCharge)
+      }
+    }
+
+    def transform2[T](data: Either[String, T]): Either[AmendmentDataFailure, T] = {
+      data match {
+        case Left(string) => Left(AmendmentDataFailure(string))
+        case Right(t)     => Right(t)
+      }
+    }
+
+    for {
+      productName <- transform2[String](subscriptionToMigrationProductName(subscription))
+      ratePlanDetails <- transform2[RatePlanDetails](subscriptionToRatePlanDetails(subscription, productName))
+      oldPrice = ratePlanDetails.currentPrice
+      newPrice <- transform1[BigDecimal](subscriptionToNewPrice(subscription))
+    } yield PriceData(
+      ratePlanDetails.currency,
+      oldPrice,
+      newPrice,
+      BillingPeriod.toString(ratePlanDetails.billingPeriod)
+    )
+  }
 }
--- lambda/src/test/scala/pricemigrationengine/migrations/Newspaper2024MigrationTest.scala
+++ lambda/src/test/scala/pricemigrationengine/migrations/Newspaper2024MigrationTest.scala
@@ -8,10 +8,328 @@ import pricemigrationengine.handlers.NotificationHandler.thereIsEnoughNotificati
 import pricemigrationengine.migrations.Newspaper2024Migration._
 import pricemigrationengine.model.CohortTableFilter.SalesforcePriceRiceCreationComplete
 
+/*
+  Correspondence between product names in Salesforce versus Zuora
+
+  -----------------------------------------------------------------
+  Product name in the Salesforce export | Product name in Zuora
+  -----------------------------------------------------------------
+  Newspaper - Home Delivery             | Newspaper Delivery
+  Newspaper - Subscription Card         | Newspaper Digital Voucher
+  Newspaper - Voucher Book              | Newspaper Voucher
+  -----------------------------------------------------------------
+ */
+
 class Newspaper2024MigrationTest extends munit.FunSuite {
+
   test("Newspaper2024Migration: Price lookup is correct") {
-    assertEquals(priceLookup("Newspaper - Home Delivery", Monthly, "Weekend"), Some(BigDecimal(31.99)))
-    assertEquals(priceLookup("Newspaper - Voucher Book", Annual, "Sixday+"), Some(BigDecimal(707.88)))
+    assertEquals(priceLookup("Newspaper Delivery", Monthly, "Weekend"), Some(BigDecimal(31.99)))
+    assertEquals(priceLookup("Newspaper Digital Voucher", Annual, "Sixday"), Some(BigDecimal(683.88)))
+    assertEquals(priceLookup("Newspaper Digital Voucher", Annual, "Sixday+"), None)
     assertEquals(priceLookup("Non existent product", Annual, "Weekend"), None)
   }
+
+  // -- product name -------------------------------------------------------
+
+  test("Newspaper2024Migration | migration product name is correct | NewspaperHomeDelivery-Monthly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/subscription.json")
+    // val invoicePreview = Fixtures.invoiceListFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/invoice-preview.json")
+    // val account = Fixtures.accountFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/account.json")
+    // val catalogue = Fixtures.productCatalogueFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/catalogue.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Delivery"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperHomeDelivery-Quarterly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Quarterly/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Delivery"))
+  }
+
+  test("Newspaper2024Migration | migration product name is correct | NewspaperSubscriptionCard-Monthly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Monthly/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Digital Voucher"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperSubscriptionCard-Quarterly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Quarterly/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Digital Voucher"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperSubscriptionCard-SemiAnnual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-SemiAnnual/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Digital Voucher"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperSubscriptionCard-Annual") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Annual/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Digital Voucher"))
+  }
+
+  test("Newspaper2024Migration | migration product name is correct | NewspaperVoucherBook-Monthly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Monthly/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Voucher"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperVoucherBook-Quarterly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Quarterly/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Voucher"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperVoucherBook-SemiAnnual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-SemiAnnual/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Voucher"))
+  }
+  test("Newspaper2024Migration | migration product name is correct | NewspaperVoucherBook-Annual") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Annual/subscription.json")
+    assertEquals(subscriptionToMigrationProductName(subscription), Right("Newspaper Voucher"))
+  }
+
+  // -- rate plan details -------------------------------------------------------
+
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperHomeDelivery-Monthly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    val ratePlanCharges = List(
+      ZuoraRatePlanCharge(
+        productRatePlanChargeId = "2c92a0ff560d311b0156136ba11539ae",
+        name = "Saturday",
+        number = "C-00347069",
+        currency = "GBP",
+        price = Some(12.37),
+        billingPeriod = Some("Month"),
+        chargedThroughDate = Some(LocalDate.of(2023, 12, 21)),
+        processedThroughDate = Some(LocalDate.of(2023, 11, 21)),
+        specificBillingPeriod = None,
+        endDateCondition = Some("Subscription_End"),
+        upToPeriodsType = None,
+        upToPeriods = None,
+        billingDay = Some("ChargeTriggerDay"),
+        triggerEvent = Some("CustomerAcceptance"),
+        triggerDate = None,
+        discountPercentage = None
+      ),
+      ZuoraRatePlanCharge(
+        productRatePlanChargeId = "2c92a0ff560d311b0156136ba0523996",
+        name = "Sunday",
+        number = "C-00347067",
+        currency = "GBP",
+        price = Some(12.72),
+        billingPeriod = Some("Month"),
+        chargedThroughDate = Some(LocalDate.of(2023, 12, 21)),
+        processedThroughDate = Some(LocalDate.of(2023, 11, 21)),
+        specificBillingPeriod = None,
+        endDateCondition = Some("Subscription_End"),
+        upToPeriodsType = None,
+        upToPeriods = None,
+        billingDay = Some("ChargeTriggerDay"),
+        triggerEvent = Some("CustomerAcceptance"),
+        triggerDate = None,
+        discountPercentage = None
+      ),
+      ZuoraRatePlanCharge(
+        productRatePlanChargeId = "2c92a0ff560d311b0156136b9fac3976",
+        name = "Digital Pack",
+        number = "C-00347064",
+        currency = "GBP",
+        price = Some(8.67),
+        billingPeriod = Some("Month"),
+        chargedThroughDate = Some(LocalDate.of(2023, 12, 21)),
+        processedThroughDate = Some(LocalDate.of(2023, 11, 21)),
+        specificBillingPeriod = None,
+        endDateCondition = Some("Subscription_End"),
+        upToPeriodsType = None,
+        upToPeriods = None,
+        billingDay = Some("ChargeTriggerDay"),
+        triggerEvent = Some("CustomerAcceptance"),
+        triggerDate = None,
+        discountPercentage = None
+      )
+    )
+    val ratePlan =
+      ZuoraRatePlan(
+        "8a1280478c3a4e1d018c41aa77651cde",
+        "Newspaper Delivery",
+        "2c92a0ff560d311b0156136b9f5c3968",
+        "Weekend+",
+        ratePlanCharges,
+        None
+      )
+    val details = RatePlanDetails(ratePlan, "Weekend+", Monthly, "GBP", BigDecimal(33.76)) // sum of the prices
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right(details))
+  }
+  /*
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperHomeDelivery-Quarterly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Quarterly/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend", Quarterly))
+  }
+
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-Monthly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Monthly/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend+", Monthly))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-Quarterly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Quarterly/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend", Quarterly))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-SemiAnnual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-SemiAnnual/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Sixday", SemiAnnual))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-Annual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Annual/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Everyday", Annual))
+  }
+
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-Monthly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Monthly/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend+", Monthly))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-Quarterly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Quarterly/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend+", Quarterly))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-SemiAnnual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-SemiAnnual/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend+", SemiAnnual))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-Annual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Annual/subscription.json")
+    val productName = subscriptionToMigrationProductName(subscription).toOption.get
+    assertEquals(subscriptionToRatePlanDetails(subscription, productName), Right("Weekend+", Annual))
+  }
+   */
+
+  // -- prices -------------------------------------------------------
+
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperHomeDelivery-Monthly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(40.99)))
+  }
+  /*
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperHomeDelivery-Quarterly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Quarterly/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(95.97)))
+  }
+
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-Monthly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Monthly/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(34.99)))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-Quarterly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Quarterly/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(77.97)))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-SemiAnnual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-SemiAnnual/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(341.94)))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperSubscriptionCard-Annual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Annual/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(779.88)))
+  }
+
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-Monthly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Monthly/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(34.99)))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-Quarterly") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Quarterly/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(104.97)))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-SemiAnnual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-SemiAnnual/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(209.94)))
+  }
+  test("Newspaper2024Migration | Rate plan name determination is correct | NewspaperVoucherBook-Annual") {
+    val subscription =
+      Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Annual/subscription.json")
+    assertEquals(subscriptionToNewPrice(subscription), Some(BigDecimal(419.88)))
+  }
+   */
+  // -- price data -------------------------------------------------------
+
+  test("Newspaper2024Migration | price data is correct | NewspaperHomeDelivery-Monthly") {
+    val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Monthly/subscription.json")
+    val priceData = PriceData("GBP", BigDecimal(33.76), BigDecimal(40.99), "Month")
+    assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+  }
+  /*
+test("Newspaper2024Migration | price data is correct | NewspaperHomeDelivery-Quarterly") {
+  val subscription = Fixtures.subscriptionFromJson("Newspaper2024/NewspaperHomeDelivery-Quarterly/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+
+test("Newspaper2024Migration | price data is correct | NewspaperSubscriptionCard-Monthly") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Monthly/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+test("Newspaper2024Migration | price data is correct | NewspaperSubscriptionCard-Quarterly") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Quarterly/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+test("Newspaper2024Migration | price data is correct | NewspaperSubscriptionCard-SemiAnnual") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-SemiAnnual/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+test("Newspaper2024Migration | price data is correct | NewspaperSubscriptionCard-Annual") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperSubscriptionCard-Annual/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+
+test("Newspaper2024Migration | price data is correct | NewspaperVoucherBook-Monthly") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Monthly/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+test("Newspaper2024Migration | price data is correct | NewspaperVoucherBook-Quarterly") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Quarterly/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+test("Newspaper2024Migration | price data is correct | NewspaperVoucherBook-SemiAnnual") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-SemiAnnual/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+test("Newspaper2024Migration | price data is correct | NewspaperVoucherBook-Annual") {
+  val subscription =
+    Fixtures.subscriptionFromJson("Newspaper2024/NewspaperVoucherBook-Annual/subscription.json")
+  val priceData = PriceData("GBP", BigDecimal(40.99), BigDecimal(40.99), "Monthly")
+  assertEquals(Newspaper2024Migration.priceData(subscription), Right(priceData))
+}
+   */
 }
