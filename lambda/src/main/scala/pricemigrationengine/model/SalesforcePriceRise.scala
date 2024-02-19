6da28f44a574336f4c737e604c3423ff1f3b6a62
Pascal Honoré <pascal@alseyn.net>
2024-02-19 18:08:22 +0000

    comments

--- lambda/src/main/scala/pricemigrationengine/model/SalesforcePriceRise.scala
+++ lambda/src/main/scala/pricemigrationengine/model/SalesforcePriceRise.scala
@@ -12,8 +12,13 @@ case class SalesforcePriceRise(
     Date_Letter_Sent__c: Option[LocalDate] = None,
     Amended_Zuora_Subscription_Id__c: Option[ZuoraSubscriptionId] = None,
     Migration_Name__c: Option[String],
-    Migration_Status__c: Option[
-      String
-    ], // The processing state of the cohort item at time of salesforce notification, including Cancellation
+    Migration_Status__c: Option[String], // [1]
     Cancellation_Reason__c: Option[String]
 )
+
+// [1] The processing state of the cohort item at time of salesforce notification
+//     and "Cancellation", if the item is bout to or has been cancelled.
+
+// Note that Cancellation_Reason__c should remain withing 255 chars. This is a limitation
+// imposed by Salesforce which came during the initial
+// integration: https://github.com/guardian/salesforce/pull/976
