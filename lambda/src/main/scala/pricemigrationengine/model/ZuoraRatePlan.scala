235e73d8df56cf4f4ab7eb5a07fde9b14443ff9e
Pascal Honoré <pascal@alseyn.net>
2024-03-07 18:21:49 +0000

    code refactoring

---
+++ lambda/src/main/scala/pricemigrationengine/model/ZuoraRatePlan.scala
@@ -0,0 +1,20 @@
+package pricemigrationengine.model
+
+import pricemigrationengine.model.OptionReader // not sure why this import is needed as should be visible implicitly
+import upickle.default._
+
+case class ZuoraRatePlan(
+    id: String,
+    productName: String,
+    productRatePlanId: String,
+    ratePlanName: String,
+    ratePlanCharges: List[ZuoraRatePlanCharge],
+    lastChangeType: Option[String] = None
+)
+
+object ZuoraRatePlan {
+  implicit val rw: ReadWriter[ZuoraRatePlan] = macroRW
+
+  def ratePlan(subscription: ZuoraSubscription, ratePlanCharge: ZuoraRatePlanCharge): Option[ZuoraRatePlan] =
+    subscription.ratePlans.find(_.ratePlanCharges.exists(_.number == ratePlanCharge.number))
+}
