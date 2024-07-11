9bbd05c048d52264117fc62b032f5124198ab004
Pascal Honoré <pascal@alseyn.net>
2024-07-11 11:24:20 +0100

    Introduce SupporterPlus2024MigrationTest

---
+++ lambda/src/test/scala/pricemigrationengine/migrations/SupporterPlus2024MigrationTest.scala
@@ -0,0 +1,17 @@
+package pricemigrationengine.migrations
+
+import pricemigrationengine.model._
+
+import java.time.LocalDate
+import pricemigrationengine.Fixtures
+import pricemigrationengine.handlers.NotificationHandler
+import pricemigrationengine.util.StartDates
+
+class SupporterPlus2024MigrationTest extends munit.FunSuite {
+  test("template") {
+    assertEquals(
+      true,
+      true
+    )
+  }
+}
