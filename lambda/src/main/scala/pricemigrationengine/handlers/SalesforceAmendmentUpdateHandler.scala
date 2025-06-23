44d3fde2c01d7fdad371eeceafc92299eb442bbe
Pascal Honoré <pascal@alseyn.net>
2025-06-23 12:27:36 +0100

    enable following steps

--- lambda/src/main/scala/pricemigrationengine/handlers/SalesforceAmendmentUpdateHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/SalesforceAmendmentUpdateHandler.scala
@@ -66,8 +66,7 @@ object SalesforceAmendmentUpdateHandler extends CohortHandler {
 
   def handle(input: CohortSpec): ZIO[Logging, Failure, HandlerOutput] = {
     MigrationType(input) match {
-      case GuardianWeekly2025 => ZIO.succeed(HandlerOutput(isComplete = true))
-      case Newspaper2025P1    => ZIO.succeed(HandlerOutput(isComplete = true))
+      case Newspaper2025P1 => ZIO.succeed(HandlerOutput(isComplete = true))
       case _ => {
         main(input).provideSome[Logging](
           EnvConfig.cohortTable.layer,
