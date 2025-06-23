c3c71b5e89da44111e4fa47524053f4203decc02
Pascal Honoré <pascal@alseyn.net>
2025-06-23 11:15:23 +0100

    Activate SalesforceNotificationDateUpdateHandler for GuardianWeekly2025

--- lambda/src/main/scala/pricemigrationengine/handlers/SalesforceNotificationDateUpdateHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/SalesforceNotificationDateUpdateHandler.scala
@@ -75,8 +75,7 @@ object SalesforceNotificationDateUpdateHandler extends CohortHandler {
 
   def handle(input: CohortSpec): ZIO[Logging, Failure, HandlerOutput] = {
     MigrationType(input) match {
-      case GuardianWeekly2025 => ZIO.succeed(HandlerOutput(isComplete = true))
-      case Newspaper2025P1    => ZIO.succeed(HandlerOutput(isComplete = true))
+      case Newspaper2025P1 => ZIO.succeed(HandlerOutput(isComplete = true))
       case _ => {
         main(input).provideSome[Logging](
           EnvConfig.cohortTable.layer,
