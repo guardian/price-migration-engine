eeb02a60fef2a504df6d7135b1e9a1c36565b8f5
Pascal Honoré <pascal@alseyn.net>
2025-07-05 16:36:20 +0100

    Allow for subscription uploads

--- lambda/src/main/scala/pricemigrationengine/handlers/CohortTableCreationHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/CohortTableCreationHandler.scala
@@ -1,6 +1,14 @@
 package pricemigrationengine.handlers
 
-import pricemigrationengine.model.{CohortSpec, Failure, GuardianWeekly2025, HandlerOutput, HomeDelivery2025, MigrationType, Newspaper2025P1}
+import pricemigrationengine.model.{
+  CohortSpec,
+  Failure,
+  GuardianWeekly2025,
+  HandlerOutput,
+  HomeDelivery2025,
+  MigrationType,
+  Newspaper2025P1
+}
 import pricemigrationengine.services._
 import zio.ZIO
 
@@ -22,8 +30,6 @@ object CohortTableCreationHandler extends CohortHandler {
 
   def handle(input: CohortSpec): ZIO[Logging, Failure, HandlerOutput] =
     MigrationType(input) match {
-      case Newspaper2025P1 => ZIO.succeed(HandlerOutput(isComplete = true))
-      case HomeDelivery2025 => ZIO.succeed(HandlerOutput(isComplete = true))
       case _ => {
         main(input).provideSome[Logging](
           EnvConfig.stage.layer,
--- lambda/src/main/scala/pricemigrationengine/handlers/SubscriptionIdUploadHandler.scala
+++ lambda/src/main/scala/pricemigrationengine/handlers/SubscriptionIdUploadHandler.scala
@@ -90,8 +90,6 @@ object SubscriptionIdUploadHandler extends CohortHandler {
 
   def handle(input: CohortSpec): ZIO[Logging, Failure, HandlerOutput] = {
     MigrationType(input) match {
-      case Newspaper2025P1  => ZIO.succeed(HandlerOutput(isComplete = true))
-      case HomeDelivery2025 => ZIO.succeed(HandlerOutput(isComplete = true))
       case _ => {
         main(input).provideSome[Logging](
           EnvConfig.cohortTable.layer,
