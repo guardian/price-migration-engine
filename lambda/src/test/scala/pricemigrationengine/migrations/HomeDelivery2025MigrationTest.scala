0a124fa280ad2549d928f7e9439718c04f689a69
Pascal Honoré <pascal@alseyn.net>
2025-07-08 12:12:13 +0100

    home delivery: test helper functions

--- lambda/src/test/scala/pricemigrationengine/migrations/HomeDelivery2025MigrationTest.scala
+++ lambda/src/test/scala/pricemigrationengine/migrations/HomeDelivery2025MigrationTest.scala
@@ -2,6 +2,8 @@ package pricemigrationengine.migrations
 
 import pricemigrationengine.model.CohortTableFilter.ReadyForEstimation
 import pricemigrationengine.model._
+import pricemigrationengine.Fixtures
+import pricemigrationengine.libs.SI2025RateplanFromSubAndInvoices
 
 import java.time.LocalDate
 
@@ -61,6 +63,89 @@ class HomeDelivery2025MigrationTest extends munit.FunSuite {
     )
     val label = HomeDelivery2025Migration.decideShouldRemoveDiscount(cohortItem)
     assertEquals(label, false)
+  }
+
+  test("priceLookUp") {
+    assertEquals(
+      HomeDelivery2025Migration.priceLookUp(HomeDelivery2025Everyday, Monthly),
+      Some(BigDecimal(83.99))
+    )
+
+    assertEquals(
+      HomeDelivery2025Migration.priceLookUp(HomeDelivery2025Sixday, SemiAnnual),
+      Some(BigDecimal(443.94))
+    )
+
+    assertEquals(
+      HomeDelivery2025Migration.priceLookUp(HomeDelivery2025Weekend, Quarterly),
+      Some(BigDecimal(104.97))
+    )
+
+    assertEquals(
+      HomeDelivery2025Migration.priceLookUp(HomeDelivery2025Saturday, Monthly),
+      Some(BigDecimal(20.99))
+    )
+
+    // And we test an undefined combination
+    assertEquals(
+      HomeDelivery2025Migration.priceLookUp(HomeDelivery2025Saturday, SemiAnnual),
+      Some(BigDecimal(125.94))
+    )
+  }
+
+  // ---------------------------------------------------
+  // A-S00252266
+  // Newspaper - Home Delivery
+  // Sixday
+  // Month
+  // val subscription = Fixtures.subscriptionFromJson("Migrations/HomeDelivery2025/A-S00252266/subscription.json")
+  // val account = Fixtures.accountFromJson("Migrations/HomeDelivery2025/A-S00252266/account.json")
+  // val invoicePreview = Fixtures.invoiceListFromJson("Migrations/HomeDelivery2025/A-S00252266/invoice-preview.json")
+
+  // ---------------------------------------------------
+  // A-S00256852
+  // Newspaper - Home Delivery
+  // Weekend
+  // Month
+  // val subscription = Fixtures.subscriptionFromJson("Migrations/HomeDelivery2025/A-S00256852/subscription.json")
+  // val account = Fixtures.accountFromJson("Migrations/HomeDelivery2025/A-S00256852/account.json")
+  // val invoicePreview = Fixtures.invoiceListFromJson("Migrations/HomeDelivery2025/A-S00256852/invoice-preview.json")
+
+  // ---------------------------------------------------
+  // GA0000464
+  // Newspaper - Home Delivery
+  // Saturday
+  // Quarter
+  // val subscription = Fixtures.subscriptionFromJson("Migrations/HomeDelivery2025/GA0000464/subscription.json")
+  // val account = Fixtures.accountFromJson("Migrations/HomeDelivery2025/GA0000464/account.json")
+  // val invoicePreview = Fixtures.invoiceListFromJson("Migrations/HomeDelivery2025/GA0000464/invoice-preview.json")
+
+  // ---------------------------------------------------
+  // GA0004508
+  // Newspaper - Home Delivery
+  // Weekend
+  // Quarter
+  // val subscription = Fixtures.subscriptionFromJson("Migrations/HomeDelivery2025/GA0004508/subscription.json")
+  // val account = Fixtures.accountFromJson("Migrations/HomeDelivery2025/GA0004508/account.json")
+  // val invoicePreview = Fixtures.invoiceListFromJson("Migrations/HomeDelivery2025/GA0004508/invoice-preview.json")
+
+  // ---------------------------------------------------
+  // GA0006731
+  // Newspaper - Home Delivery
+  // Saturday
+  // Month
+  // val subscription = Fixtures.subscriptionFromJson("Migrations/HomeDelivery2025/GA0006731/subscription.json")
+  // val account = Fixtures.accountFromJson("Migrations/HomeDelivery2025/GA0006731/account.json")
+  // val invoicePreview = Fixtures.invoiceListFromJson("Migrations/HomeDelivery2025/GA0006731/invoice-preview.json")
+
+  test("decideProductType (276579)") {
+    val subscription = Fixtures.subscriptionFromJson("Migrations/Newspaper2025P1/276579/subscription.json")
+    val invoicePreview = Fixtures.invoiceListFromJson("Migrations/Newspaper2025P1/276579/invoice-preview.json")
+    val ratePlan = SI2025RateplanFromSubAndInvoices.determineRatePlan(subscription, invoicePreview).get
+    assertEquals(
+      Newspaper2025P1Migration.decideProductType(ratePlan),
+      Some(Newspaper2025P1Voucher)
+    )
   }
 
 }
