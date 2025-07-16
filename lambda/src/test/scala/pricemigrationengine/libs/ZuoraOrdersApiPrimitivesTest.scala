9345287ec365c5e141ff1a376c1ca1a5832c0d6d
Pascal Honoré <pascal@alseyn.net>
2025-07-16 12:19:20 +0200

    ZuoraOrdersApiPrimitives.ratePlanChargesToChargeOverrides

--- lambda/src/test/scala/pricemigrationengine/libs/ZuoraOrdersApiPrimitivesTest.scala
+++ lambda/src/test/scala/pricemigrationengine/libs/ZuoraOrdersApiPrimitivesTest.scala
@@ -1,6 +1,9 @@
 package pricemigrationengine.libs
 
+import pricemigrationengine.model.{ZuoraRatePlan, ZuoraRatePlanCharge}
 import ujson.Value
+
+import java.time.LocalDate
 
 class ZuoraOrdersAPIPrimitivesTest extends munit.FunSuite {
 
@@ -315,5 +318,237 @@ class ZuoraOrdersAPIPrimitivesTest extends munit.FunSuite {
         |    }
         |}""".stripMargin
     )
+  }
+
+  test("ratePlanChargesToChargeOverrides") {
+
+    // The following rate plan was taken from the Newspaper2025P1 test suite.
+    // We stole it from there to test ratePlanChargesToChargeOverrides
+    val ratePlan = ZuoraRatePlan(
+      id = "8a12910195aa4f620195b0e2bc0c139d",
+      productName = "Newspaper Voucher",
+      productRatePlanId = "2c92a0fc56fe26ba0157040c5ea17f6a",
+      ratePlanName = "Sixday+",
+      ratePlanCharges = List(
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0ff56fe33f5015709cdedbd246b",
+          name = "Tuesday",
+          number = "C-05403277",
+          currency = "GBP",
+          price = Some(BigDecimal(8.96)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        ),
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0ff56fe33f3015709d10a436f52",
+          name = "Digipack",
+          number = "C-05403276",
+          currency = "GBP",
+          price = Some(BigDecimal(2.0)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        ),
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0fe56fe33ff015704325d87494c",
+          name = "Monday",
+          number = "C-05403275",
+          currency = "GBP",
+          price = Some(BigDecimal(8.96)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        ),
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0fd56fe26b6015709d078df4a80",
+          name = "Saturday",
+          number = "C-05403274",
+          currency = "GBP",
+          price = Some(BigDecimal(12.19)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        ),
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0fd56fe26b6015709cfc1500a2e",
+          name = "Friday",
+          number = "C-05403273",
+          currency = "GBP",
+          price = Some(BigDecimal(8.96)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        ),
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0fd56fe26b6015709ced61a032e",
+          name = "Wednesday",
+          number = "C-05403272",
+          currency = "GBP",
+          price = Some(BigDecimal(8.96)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        ),
+        ZuoraRatePlanCharge(
+          productRatePlanChargeId = "2c92a0fc56fe26ba015709cf4bbd3d1c",
+          name = "Thursday",
+          number = "C-05403271",
+          currency = "GBP",
+          price = Some(BigDecimal(8.96)),
+          billingPeriod = Some("Month"),
+          chargedThroughDate = Some(LocalDate.of(2025, 7, 8)),
+          processedThroughDate = Some(LocalDate.of(2025, 6, 8)),
+          specificBillingPeriod = None,
+          endDateCondition = Some("Subscription_End"),
+          upToPeriodsType = None,
+          upToPeriods = None,
+          billingDay = Some("ChargeTriggerDay"),
+          triggerEvent = Some("CustomerAcceptance"),
+          triggerDate = None,
+          discountPercentage = None,
+          originalOrderDate = Some(LocalDate.of(2024, 3, 29)),
+          effectiveStartDate = Some(LocalDate.of(2024, 5, 8)),
+          effectiveEndDate = Some(LocalDate.of(2025, 9, 8))
+        )
+      ),
+      lastChangeType = Some("Add")
+    )
+
+    val json = ZuoraOrdersApiPrimitives.ratePlanChargesToChargeOverrides(
+      ratePlan.ratePlanCharges,
+      BigDecimal(1.5)
+    )
+
+    val jsonstrpp = ujson.write(json, indent = 4)
+
+    assertEquals(
+      jsonstrpp,
+      """[
+        |    {
+        |        "productRatePlanChargeId": "2c92a0ff56fe33f5015709cdedbd246b",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 13.44
+        |            }
+        |        }
+        |    },
+        |    {
+        |        "productRatePlanChargeId": "2c92a0ff56fe33f3015709d10a436f52",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 3
+        |            }
+        |        }
+        |    },
+        |    {
+        |        "productRatePlanChargeId": "2c92a0fe56fe33ff015704325d87494c",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 13.44
+        |            }
+        |        }
+        |    },
+        |    {
+        |        "productRatePlanChargeId": "2c92a0fd56fe26b6015709d078df4a80",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 18.285
+        |            }
+        |        }
+        |    },
+        |    {
+        |        "productRatePlanChargeId": "2c92a0fd56fe26b6015709cfc1500a2e",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 13.44
+        |            }
+        |        }
+        |    },
+        |    {
+        |        "productRatePlanChargeId": "2c92a0fd56fe26b6015709ced61a032e",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 13.44
+        |            }
+        |        }
+        |    },
+        |    {
+        |        "productRatePlanChargeId": "2c92a0fc56fe26ba015709cf4bbd3d1c",
+        |        "pricing": {
+        |            "recurringFlatFee": {
+        |                "listPrice": 13.44
+        |            }
+        |        }
+        |    }
+        |]""".stripMargin
+    )
+
   }
 }
