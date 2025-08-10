425095018b47a06c4f3890b71dbc15b15ec06a53
Pascal Honoré <pascal@alseyn.net>
2025-08-10 20:06:41 +0100

    Decommission AmendmentFailed

--- lambda/src/main/scala/pricemigrationengine/model/CohortTableFilter.scala
+++ lambda/src/main/scala/pricemigrationengine/model/CohortTableFilter.scala
@@ -44,14 +44,11 @@ object CohortTableFilter {
 
   // ++++++++++ Exceptional states ++++++++++
 
-  case object AmendmentFailed extends CohortTableFilter { override val value: String = "AmendmentFailed" }
-
   case object DoNotProcessUntil extends CohortTableFilter { override val value: String = "DoNotProcessUntil" }
 
   // Set of all states.  Remember to update when adding a state.
   val all: Set[CohortTableFilter] = Set(
     AmendmentComplete,
-    AmendmentFailed,
     AmendmentWrittenToSalesforce,
     Cancelled,
     EstimationComplete,
