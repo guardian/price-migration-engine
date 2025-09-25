ff22d645ea859d49456369ea7de71c52b99572de
Pascal Honoré <pascal@alseyn.net>
2025-09-25 12:31:21 +0100

    deserialise JSON float to BigDecimal for SalesforcePriceRise

--- lambda/src/test/scala/pricemigrationengine/service/SalesforceClientLiveTest.scala
+++ lambda/src/test/scala/pricemigrationengine/service/SalesforceClientLiveTest.scala
@@ -1,9 +1,9 @@
 package pricemigrationengine.service
 
 import java.time.LocalDate
-
-import pricemigrationengine.model.SalesforcePriceRise
-import pricemigrationengine.services.SalesforceClientLive
+import pricemigrationengine.model.{SalesforcePriceRise, ZuoraSubscriptionId}
+import pricemigrationengine.services.{SalesforceClientLive, SalesforcePriceRiseCreationResponse}
+import upickle.default._
 
 class SalesforceClientLiveTest extends munit.FunSuite {
   test("SalesforceClientLive should serialise SalesforcePriceRise with all fields") {
@@ -12,8 +12,8 @@ class SalesforceClientLiveTest extends munit.FunSuite {
         SalesforcePriceRise(
           Name = Some("name"),
           Buyer__c = Some("buyer"),
-          Current_Price_Today__c = Some(1.23),
-          Guardian_Weekly_New_Price__c = Some(1.99),
+          Current_Price_Today__c = Some(BigDecimal(1.23)),
+          Guardian_Weekly_New_Price__c = Some(BigDecimal(1.99)),
           Price_Rise_Date__c = Some(LocalDate.of(2020, 1, 1)),
           SF_Subscription__c = Some("subscriptionId"),
           Date_Letter_Sent__c = Some(LocalDate.of(2020, 1, 2)),
@@ -25,8 +25,8 @@ class SalesforceClientLiveTest extends munit.FunSuite {
       """{
         |  "Name": "name",
         |  "Buyer__c": "buyer",
-        |  "Current_Price_Today__c": "1.23",
-        |  "Guardian_Weekly_New_Price__c": "1.99",
+        |  "Current_Price_Today__c": 1.23,
+        |  "Guardian_Weekly_New_Price__c": 1.99,
         |  "Price_Rise_Date__c": "2020-01-01",
         |  "SF_Subscription__c": "subscriptionId",
         |  "Date_Letter_Sent__c": "2020-01-02",
@@ -52,6 +52,30 @@ class SalesforceClientLiveTest extends munit.FunSuite {
         |  "Migration_Status__c": "EstimationComplete",
         |  "Cancellation_Reason__c": "Error 1"
         |}""".stripMargin
+    )
+  }
+  test("SalesforceClientLive should deserialise correctly the JSON object from Salesforce") {
+    implicit val bigDecimalRW: ReadWriter[BigDecimal] =
+      readwriter[ujson.Value].bimap[BigDecimal](
+        bd => ujson.Num(bd.toDouble), // write
+        js => js.num // read as number
+      )
+    implicit val localDateRW: ReadWriter[LocalDate] =
+      readwriter[String].bimap[LocalDate](_.toString, LocalDate.parse)
+    implicit val salesforcePriceRiseRW: ReadWriter[SalesforcePriceRise] = macroRW
+
+    val rawJSON = """{
+    |  "Migration_Name__c": "cohortName",
+    |  "Migration_Status__c": "EstimationComplete",
+    |  "Cancellation_Reason__c": "Error 1",
+    |  "Current_Price_Today__c":311.88,
+    |  "Guardian_Weekly_New_Price__c": 335.88
+    |}""".stripMargin
+
+    val priceRiseObject = read[SalesforcePriceRise](rawJSON)
+    assertEquals(
+      priceRiseObject.Guardian_Weekly_New_Price__c,
+      Some(BigDecimal(335.88))
     )
   }
 }
