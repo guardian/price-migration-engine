da093d57a837c484bf32e9e51879b39bbd5345b9
Pascal Honoré <pascal@alseyn.net>
2025-09-25 21:29:21 +0100

    N4 test file

---
+++ lambda/src/test/scala/pricemigrationengine/migrations/ProductMigration2025N4MigrationTest.scala
@@ -0,0 +1,13 @@
+package pricemigrationengine.migrations
+
+import pricemigrationengine.model.CohortTableFilter.ReadyForEstimation
+import pricemigrationengine.model._
+import pricemigrationengine.Fixtures
+
+import java.time.LocalDate
+
+class ProductMigration2025N4MigrationTest extends munit.FunSuite {
+  test("true") {
+    assertEquals(true, true)
+  }
+}
