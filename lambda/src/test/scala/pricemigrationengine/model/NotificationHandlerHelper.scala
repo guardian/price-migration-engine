2dd40018b5cf972777cf582ce01c401368299c16
Pascal Honoré <pascal@alseyn.net>
2025-10-26 19:34:10 +0000

    educational tests

--- lambda/src/test/scala/pricemigrationengine/model/NotificationHandlerHelper.scala
+++ lambda/src/test/scala/pricemigrationengine/model/NotificationHandlerHelper.scala
@@ -3,6 +3,14 @@ package pricemigrationengine.handlers
 import pricemigrationengine.model._
 
 class NotificationHandlerHelperTest extends munit.FunSuite {
+  test("basic scala used in assertNonTrivialValue") {
+    assertEquals("".isEmpty, true)
+    assertEquals("thing".isEmpty, false)
+  }
+  test("basic scala used in messageIsWellFormed") {
+    assertEquals(List(true, false).forall(identity), false)
+    assertEquals(List(true, true).forall(identity), true)
+  }
   test("assertNonTrivialValue") {
     assertEquals(NotificationHandlerHelper.assertNonTrivialValue("value name", None), false)
     assertEquals(NotificationHandlerHelper.assertNonTrivialValue("value name", Some("")), false)
