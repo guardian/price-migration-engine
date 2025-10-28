08a1f143c08d49b72e7cd63afdacfedea0000226
Pascal Honoré <pascal@alseyn.net>
2025-10-28 18:07:16 +0000

    membership payload test 1

--- lambda/src/test/scala/pricemigrationengine/migrations/Membership2025MigrationTest.scala
+++ lambda/src/test/scala/pricemigrationengine/migrations/Membership2025MigrationTest.scala
@@ -1,6 +1,7 @@
 package pricemigrationengine.migrations
 
 import pricemigrationengine.Fixtures
+import pricemigrationengine.model.CohortTableFilter
 import pricemigrationengine.model._
 
 import java.time.{Instant, LocalDate}
@@ -54,6 +55,119 @@ class Membership2025MigrationTest extends munit.FunSuite {
     assertEquals(
       Membership2025Migration.priceData(subscription, invoicePreview),
       Right(PriceData("GBP", BigDecimal(5), BigDecimal(10), "Month"))
+    )
+  }
+
+  test("amendment payload for sub1") {
+    val subscription = Fixtures.subscriptionFromJson("Migrations/Membership2025/sub1/subscription.json")
+    val account = Fixtures.accountFromJson("Migrations/Membership2025/sub1/account.json")
+    val invoicePreview = Fixtures.invoiceListFromJson("Migrations/Membership2025/sub1/invoice-preview.json")
+
+    // PriceData from the above test: PriceData("GBP", BigDecimal(7), BigDecimal(10), "Month")
+
+    val cohortItem: CohortItem = CohortItem(
+      subscriptionName = "subscriptionNumber",
+      processingStage = CohortTableFilter.NotificationSendDateWrittenToSalesforce,
+      currency = Some("GBP"),
+
+      // Pre migration price
+      oldPrice = Some(BigDecimal(7)),
+
+      // Price derived from the Estimation step, without capping
+      estimatedNewPrice = Some(BigDecimal(10)),
+
+      // Price (with possible capping) used in the communication to the user and sent to Salesforce
+      commsPrice = Some(BigDecimal(10)),
+
+      //
+      billingPeriod = Some("Month")
+    )
+
+    val payload = Membership2025Migration.amendmentOrderPayload(
+      cohortItem,
+      LocalDate.of(2025, 10, 28),
+      "accountNumber",
+      "subscriptionNumber",
+      LocalDate.of(2025, 11, 17),
+      subscription,
+      BigDecimal(10),
+      invoicePreview,
+    )
+
+    assertEquals(
+      payload,
+      Right(
+        ujson.read(
+          s"""{
+             |    "orderDate": "2025-10-28",
+             |    "existingAccountNumber": "accountNumber",
+             |    "subscriptions": [
+             |        {
+             |            "subscriptionNumber": "subscriptionNumber",
+             |            "orderActions": [
+             |                {
+             |                    "type": "RemoveProduct",
+             |                    "triggerDates": [
+             |                        {
+             |                            "name": "ContractEffective",
+             |                            "triggerDate": "2025-11-17"
+             |                        },
+             |                        {
+             |                            "name": "ServiceActivation",
+             |                            "triggerDate": "2025-11-17"
+             |                        },
+             |                        {
+             |                            "name": "CustomerAcceptance",
+             |                            "triggerDate": "2025-11-17"
+             |                        }
+             |                    ],
+             |                    "removeProduct": {
+             |                        "ratePlanId": "8a128182980d17de0198165347296f5a"
+             |                    }
+             |                },
+             |                {
+             |                    "type": "AddProduct",
+             |                    "triggerDates": [
+             |                        {
+             |                            "name": "ContractEffective",
+             |                            "triggerDate": "2025-11-17"
+             |                        },
+             |                        {
+             |                            "name": "ServiceActivation",
+             |                            "triggerDate": "2025-11-17"
+             |                        },
+             |                        {
+             |                            "name": "CustomerAcceptance",
+             |                            "triggerDate": "2025-11-17"
+             |                        }
+             |                    ],
+             |                    "addProduct": {
+             |                        "productRatePlanId": "8a1287c586832d250186a2040b1548fe",
+             |                        "chargeOverrides": [
+             |                            {
+             |                                "productRatePlanChargeId": "8a12800986832d1d0186a20bf5136471",
+             |                                "pricing": {
+             |                                    "recurringFlatFee": {
+             |                                        "listPrice": 10
+             |                                    }
+             |                                },
+             |                                "billing": {
+             |                                    "billingPeriod": "Month"
+             |                                }
+             |                            }
+             |                        ]
+             |                    }
+             |                }
+             |            ]
+             |        }
+             |    ],
+             |    "processingOptions": {
+             |        "runBilling": false,
+             |        "collectPayment": false
+             |    }
+             |}""".stripMargin
+        )
+      )
     )
   }
 }
