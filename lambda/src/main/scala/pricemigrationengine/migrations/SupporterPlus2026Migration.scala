78576866da809fa28eda3842a9663e3e6eb463a6
Pascal Honoré <pascal@alseyn.net>
2026-07-02 22:33:46 +0100

    test fixtures

--- lambda/src/main/scala/pricemigrationengine/migrations/SupporterPlus2026Migration.scala
+++ lambda/src/main/scala/pricemigrationengine/migrations/SupporterPlus2026Migration.scala
@@ -71,12 +71,33 @@ object SupporterPlus2026Migration {
   // - amendmentOrderPayload is used in the Amendment handler
   // ------------------------------------------------
 
+  def logValue[T](label: String)(value: T): T = {
+    println(s"$label: $value")
+    value
+  }
+
   def priceData(
       subscription: ZuoraSubscription,
       invoiceList: ZuoraInvoiceList,
   ): Either[DataExtractionFailure, PriceData] = {
-    // not yet implemented
-    ???
+    val priceDataOpt = for {
+      ratePlan <- SI2025RateplanFromSubAndInvoices
+        .determineRatePlan(subscription, invoiceList)
+        .map(logValue("ratePlan"))
+      currency <- SI2025Extractions.determineCurrency(ratePlan).map(logValue("currency"))
+      billingPeriod <- SI2025Extractions.determineBillingPeriod(ratePlan).map(logValue("billingPeriod"))
+      oldPrice = logValue("oldPrice")(SI2025Extractions.determineOldPrice(ratePlan))
+      newPrice <- priceGridNewPrices.get((billingPeriod, currency)).map(logValue("newPrice"))
+    } yield PriceData(currency, oldPrice, newPrice, BillingPeriod.toString(billingPeriod))
+    priceDataOpt match {
+      case Some(pricedata) => Right(pricedata)
+      case None            =>
+        Left(
+          DataExtractionFailure(
+            s"[e0254802] Could not determine PriceData for subscription ${subscription.subscriptionNumber}"
+          )
+        )
+    }
   }
 
   def amendmentOrderPayload(cohortItem: CohortItem): Either[Failure, Value] = {
