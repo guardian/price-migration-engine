bc1cecfe3b3f256d496460e523efa77c07699e69
Pascal Honoré <pascal@alseyn.net>
2026-07-19 07:19:25 +0100

    remove unused code (zuora catalogue)

--- lambda/src/main/scala/pricemigrationengine/model/ZuoraProductCatalogue.scala
+++ lambda/src/main/scala/pricemigrationengine/model/ZuoraProductCatalogue.scala
@@ -5,40 +5,8 @@ import upickle.default.{ReadWriter, macroRW}
 case class ZuoraProductCatalogue(products: Set[ZuoraProduct], nextPage: Option[String] = None)
 
 object ZuoraProductCatalogue {
-
   implicit val rw: ReadWriter[ZuoraProductCatalogue] = macroRW
-
   def empty: ZuoraProductCatalogue = ZuoraProductCatalogue(products = Set.empty)
-
-  private val gwRatePlanNames = List("Guardian Weekly - Domestic", "Guardian Weekly - ROW")
-
-  def homeDeliveryRatePlans(catalogue: ZuoraProductCatalogue): Seq[ZuoraProductRatePlan] = {
-    val prices = for {
-      product <- catalogue.products.filter(_.name == "Newspaper Delivery")
-      productRatePlan <- product.productRatePlans.filter(_.status != "Expired")
-    } yield productRatePlan
-
-    prices.toSeq
-  }
-
-  def newGuardianWeeklyRatePlans(catalogue: ZuoraProductCatalogue): Seq[ZuoraProductRatePlan] = {
-    val prices = for {
-      product <- catalogue.products.filter(gwRatePlanNames contains _.name)
-      productRatePlan <- product.productRatePlans
-    } yield productRatePlan
-
-    prices.toSeq
-  }
-
-  def productPricingMap(catalogue: ZuoraProductCatalogue): ZuoraPricingData = {
-    val priceMapping = for {
-      product <- catalogue.products
-      productRatePlan <- product.productRatePlans.filter(x => x.status != "Expired" || x.name == "Echo-Legacy")
-      productRatePlanCharge <- productRatePlan.productRatePlanCharges
-    } yield productRatePlanCharge.id -> productRatePlanCharge
-
-    priceMapping.toMap
-  }
 }
 
 case class ZuoraProduct(
@@ -75,7 +43,4 @@ case class ZuoraPricing(currency: Currency, price: Option[BigDecimal], hasBeenPr
 
 object ZuoraPricing {
   implicit val rw: ReadWriter[ZuoraPricing] = macroRW
-
-  def pricing(productRatePlanCharge: ZuoraProductRatePlanCharge, currency: Currency): Option[ZuoraPricing] =
-    productRatePlanCharge.pricing.find(_.currency == currency)
 }
