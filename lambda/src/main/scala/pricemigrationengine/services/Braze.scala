dd2604dfdd26c2e180f898c46fd034809f0f5cc2
Pascal Honoré <pascal@alseyn.net>
2026-09-06 18:25:54 +0100

    More Braze refactoring

--- lambda/src/main/scala/pricemigrationengine/services/Braze.scala
+++ lambda/src/main/scala/pricemigrationengine/services/Braze.scala
@@ -1,15 +1,15 @@
 package pricemigrationengine.services
 
-import pricemigrationengine.model.EmailSenderFailure
+import pricemigrationengine.model.BrazeFailure
 import pricemigrationengine.model.membershipworkflow.BrazeMessage
 import zio.ZIO
 
 trait Braze {
-  def sendMessage(message: BrazeMessage): ZIO[Any, EmailSenderFailure, Unit]
+  def sendMessage(message: BrazeMessage): ZIO[Any, BrazeFailure, Unit]
 }
 
 object Braze {
-  def sendMessage(message: BrazeMessage): ZIO[Braze, EmailSenderFailure, Unit] = {
+  def sendMessage(message: BrazeMessage): ZIO[Braze, BrazeFailure, Unit] = {
     ZIO.environmentWithZIO(_.get.sendMessage(message))
   }
 }
