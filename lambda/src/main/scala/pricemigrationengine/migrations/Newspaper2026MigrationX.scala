1d9f332a4f98121c25de0e4f1481408643fd9ac8
Pascal Honoré <pascal@alseyn.net>
2026-09-07 14:08:41 +0100

    reverse NxFulfillment and NxPackage to help with review

--- lambda/src/main/scala/pricemigrationengine/migrations/Newspaper2026MigrationX.scala
+++ lambda/src/main/scala/pricemigrationengine/migrations/Newspaper2026MigrationX.scala
@@ -20,53 +20,53 @@ object SaturdayBasicAndPlus extends NxPackage
 
 object Newspaper2026MigrationX {
 
-  val priceGridNewPricesMonthlies: Map[(NxFulfillment, NxPackage), BigDecimal] = Map(
-    (Voucher, EverydayBasicAndPlus) -> BigDecimal(72.99),
-    (Voucher, SixdayBasicAndPlus) -> BigDecimal(64.99),
-    (Voucher, WeekendBasicAndPlus) -> BigDecimal(29.99),
-    (Voucher, SaturdayBasicAndPlus) -> BigDecimal(16.99),
-    (HomeDelivery, EverydayBasicAndPlus) -> BigDecimal(88.99),
-    (HomeDelivery, SixdayBasicAndPlus) -> BigDecimal(77.99),
-    (HomeDelivery, WeekendBasicAndPlus) -> BigDecimal(36.99),
-    (HomeDelivery, SaturdayBasicAndPlus) -> BigDecimal(21.99),
+  val priceGridNewPricesMonthlies: Map[(NxPackage, NxFulfillment), BigDecimal] = Map(
+    (EverydayBasicAndPlus, Voucher) -> BigDecimal(72.99),
+    (SixdayBasicAndPlus, Voucher) -> BigDecimal(64.99),
+    (WeekendBasicAndPlus, Voucher) -> BigDecimal(29.99),
+    (SaturdayBasicAndPlus, Voucher) -> BigDecimal(16.99),
+    (EverydayBasicAndPlus, HomeDelivery) -> BigDecimal(88.99),
+    (SixdayBasicAndPlus, HomeDelivery) -> BigDecimal(77.99),
+    (WeekendBasicAndPlus, HomeDelivery) -> BigDecimal(36.99),
+    (SaturdayBasicAndPlus, HomeDelivery) -> BigDecimal(21.99),
   )
-  val priceGridNewPricesQuarterlies: Map[(NxFulfillment, NxPackage), BigDecimal] = Map(
-    (Voucher, EverydayBasicAndPlus) -> BigDecimal(218.97),
-    (Voucher, SixdayBasicAndPlus) -> BigDecimal(194.97),
-    (Voucher, WeekendBasicAndPlus) -> BigDecimal(89.97),
-    (Voucher, SaturdayBasicAndPlus) -> BigDecimal(50.97),
-    (HomeDelivery, EverydayBasicAndPlus) -> BigDecimal(266.97),
-    (HomeDelivery, SixdayBasicAndPlus) -> BigDecimal(233.97),
-    (HomeDelivery, WeekendBasicAndPlus) -> BigDecimal(110.97),
-    (HomeDelivery, SaturdayBasicAndPlus) -> BigDecimal(65.97),
+  val priceGridNewPricesQuarterlies: Map[(NxPackage, NxFulfillment), BigDecimal] = Map(
+    (EverydayBasicAndPlus, Voucher) -> BigDecimal(218.97),
+    (SixdayBasicAndPlus, Voucher) -> BigDecimal(194.97),
+    (WeekendBasicAndPlus, Voucher) -> BigDecimal(89.97),
+    (SaturdayBasicAndPlus, Voucher) -> BigDecimal(50.97),
+    (EverydayBasicAndPlus, HomeDelivery) -> BigDecimal(266.97),
+    (SixdayBasicAndPlus, HomeDelivery) -> BigDecimal(233.97),
+    (WeekendBasicAndPlus, HomeDelivery) -> BigDecimal(110.97),
+    (SaturdayBasicAndPlus, HomeDelivery) -> BigDecimal(65.97),
   )
-  val priceGridNewPricesSemiannuals: Map[(NxFulfillment, NxPackage), BigDecimal] = Map(
-    (Voucher, EverydayBasicAndPlus) -> BigDecimal(437.94),
-    (Voucher, SixdayBasicAndPlus) -> BigDecimal(389.94),
-    (Voucher, WeekendBasicAndPlus) -> BigDecimal(179.9),
-    (Voucher, SaturdayBasicAndPlus) -> BigDecimal(101.94),
-    (HomeDelivery, EverydayBasicAndPlus) -> BigDecimal(533.94),
-    (HomeDelivery, SixdayBasicAndPlus) -> BigDecimal(467.94),
-    (HomeDelivery, WeekendBasicAndPlus) -> BigDecimal(221.94),
-    (HomeDelivery, SaturdayBasicAndPlus) -> BigDecimal(131.94),
+  val priceGridNewPricesSemiannuals: Map[(NxPackage, NxFulfillment), BigDecimal] = Map(
+    (EverydayBasicAndPlus, Voucher) -> BigDecimal(437.94),
+    (SixdayBasicAndPlus, Voucher) -> BigDecimal(389.94),
+    (WeekendBasicAndPlus, Voucher) -> BigDecimal(179.9),
+    (SaturdayBasicAndPlus, Voucher) -> BigDecimal(101.94),
+    (EverydayBasicAndPlus, HomeDelivery) -> BigDecimal(533.94),
+    (SixdayBasicAndPlus, HomeDelivery) -> BigDecimal(467.94),
+    (WeekendBasicAndPlus, HomeDelivery) -> BigDecimal(221.94),
+    (SaturdayBasicAndPlus, HomeDelivery) -> BigDecimal(131.94),
   )
-  val priceGridNewPricesAnnuals: Map[(NxFulfillment, NxPackage), BigDecimal] = Map(
-    (Voucher, EverydayBasicAndPlus) -> BigDecimal(875.88),
-    (Voucher, SixdayBasicAndPlus) -> BigDecimal(779.88),
-    (Voucher, WeekendBasicAndPlus) -> BigDecimal(359.88),
-    (Voucher, SaturdayBasicAndPlus) -> BigDecimal(203.88),
-    (HomeDelivery, EverydayBasicAndPlus) -> BigDecimal(1067.88),
-    (HomeDelivery, SixdayBasicAndPlus) -> BigDecimal(935.88),
-    (HomeDelivery, WeekendBasicAndPlus) -> BigDecimal(443.88),
-    (HomeDelivery, SaturdayBasicAndPlus) -> BigDecimal(263.88),
+  val priceGridNewPricesAnnuals: Map[(NxPackage, NxFulfillment), BigDecimal] = Map(
+    (EverydayBasicAndPlus, Voucher) -> BigDecimal(875.88),
+    (SixdayBasicAndPlus, Voucher) -> BigDecimal(779.88),
+    (WeekendBasicAndPlus, Voucher) -> BigDecimal(359.88),
+    (SaturdayBasicAndPlus, Voucher) -> BigDecimal(203.88),
+    (EverydayBasicAndPlus, HomeDelivery) -> BigDecimal(1067.88),
+    (SixdayBasicAndPlus, HomeDelivery) -> BigDecimal(935.88),
+    (WeekendBasicAndPlus, HomeDelivery) -> BigDecimal(443.88),
+    (SaturdayBasicAndPlus, HomeDelivery) -> BigDecimal(263.88),
   )
 
   def getNewPrice(billingPeriod: BillingPeriod, fullfilment: NxFulfillment, pack: NxPackage): Option[BigDecimal] = {
     billingPeriod match {
-      case Monthly    => priceGridNewPricesMonthlies.get(fullfilment, pack)
-      case Quarterly  => priceGridNewPricesQuarterlies.get(fullfilment, pack)
-      case SemiAnnual => priceGridNewPricesSemiannuals.get(fullfilment, pack)
-      case Annual     => priceGridNewPricesAnnuals.get(fullfilment, pack)
+      case Monthly    => priceGridNewPricesMonthlies.get(pack, fullfilment)
+      case Quarterly  => priceGridNewPricesQuarterlies.get(pack, fullfilment)
+      case SemiAnnual => priceGridNewPricesSemiannuals.get(pack, fullfilment)
+      case Annual     => priceGridNewPricesAnnuals.get(pack, fullfilment)
     }
   }
 
