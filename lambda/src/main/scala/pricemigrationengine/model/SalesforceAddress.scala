755b63734b93e66c61120ee202f87a2b0f3cf4a6
Pascal Honoré <pascal@alseyn.net>
2026-09-15 18:53:41 +0100

    refactorings

--- lambda/src/main/scala/pricemigrationengine/model/SalesforceAddress.scala
+++ lambda/src/main/scala/pricemigrationengine/model/SalesforceAddress.scala
@@ -7,3 +7,11 @@ case class SalesforceAddress(
     postalCode: Option[String],
     country: Option[String]
 )
+
+object SalesforceAddress {
+  // This value exist as the default empty NotificationAddress
+  // The reason for the Some(""), at opposition to None, is that the fields are used
+  // in a for comprehension and None would short circuit the entire process, whereas Some("")
+  // allows for empty values in cases the field wasn't required
+  val addressWithEmptyStrings = SalesforceAddress(Some(""), Some(""), Some(""), Some(""), Some(""))
+}
