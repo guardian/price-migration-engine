b8740faa10857c1b9cc2c6b9ef79cbdf75059523
Pascal Honoré <pascal@alseyn.net>
2026-09-16 08:00:06 +0100

    add tests

--- lambda/src/test/scala/pricemigrationengine/model/NotificationHandlerHelperTest.scala
+++ lambda/src/test/scala/pricemigrationengine/model/NotificationHandlerHelperTest.scala
@@ -16,4 +16,95 @@ class NotificationHandlerHelperTest extends munit.FunSuite {
     assertEquals(NotificationHandlerHelper.isNonTrivialValue(Some("")), false)
     assertEquals(NotificationHandlerHelper.isNonTrivialValue(Some("thing")), true)
   }
+
+  test("SalesforceAddress") {
+    val address1 = SalesforceAddress(
+      street = Some("street"),
+      city = Some("city"),
+      state = Some("state"),
+      postalCode = Some("postalCode"),
+      country = Some("country")
+    )
+    val contact = SalesforceContact(
+      Id = "Id",
+      IdentityID__c = None,
+      Email = None,
+      Salutation = None,
+      FirstName = None,
+      LastName = None,
+      OtherAddress = Some(address1),
+      MailingAddress = None
+    )
+    // In this case OtherAddress if fine, going to be selected
+    assertEquals(NotificationHandlerHelper.decideSalesforceAddress(contact), address1)
+  }
+
+  test("SalesforceAddress") {
+    val address1 = SalesforceAddress(
+      street = Some("street"),
+      city = None,
+      state = Some("state"),
+      postalCode = Some("postalCode"),
+      country = Some("country")
+    )
+    val address2 = SalesforceAddress(
+      street = Some("street2"),
+      city = None,
+      state = Some("state2"),
+      postalCode = Some("postalCode2"),
+      country = Some("country2")
+    )
+    val contact = SalesforceContact(
+      Id = "Id",
+      IdentityID__c = None,
+      Email = None,
+      Salutation = None,
+      FirstName = None,
+      LastName = None,
+      OtherAddress = Some(address1),
+      MailingAddress = Some(address2)
+    )
+    // In this case OtherAddress is missing city, so MailingAddress, which is defined,
+    // is going to be selected, even if itself is also missing city
+    assertEquals(NotificationHandlerHelper.decideSalesforceAddress(contact), address2)
+  }
+
+  test("SalesforceAddress") {
+    val address1 = SalesforceAddress(
+      street = Some("street"),
+      city = None,
+      state = Some("state"),
+      postalCode = Some("postalCode"),
+      country = Some("country")
+    )
+    val contact = SalesforceContact(
+      Id = "Id",
+      IdentityID__c = None,
+      Email = None,
+      Salutation = None,
+      FirstName = None,
+      LastName = None,
+      OtherAddress = Some(address1),
+      MailingAddress = None
+    )
+    // Here we default to SalesforceAddress.addressWithEmptyStrings
+    // because OtherAddress is not selectable and MailingAddress is missing
+    assertEquals(NotificationHandlerHelper.decideSalesforceAddress(contact), SalesforceAddress.addressWithEmptyStrings)
+  }
+
+  test("SalesforceAddress") {
+    val contact = SalesforceContact(
+      Id = "Id",
+      IdentityID__c = None,
+      Email = None,
+      Salutation = None,
+      FirstName = None,
+      LastName = None,
+      OtherAddress = None,
+      MailingAddress = None
+    )
+    // Here we default to SalesforceAddress.addressWithEmptyStrings
+    assertEquals(NotificationHandlerHelper.decideSalesforceAddress(contact), SalesforceAddress.addressWithEmptyStrings)
+  }
+
 }
