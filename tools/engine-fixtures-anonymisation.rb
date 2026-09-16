24ccd78dbd4e13df2c5f42a7f1ef6ee312d8b6ff
Pascal Honoré <pascal@alseyn.net>
2026-09-16 22:33:01 +0100

    add information to text fixtures

--- tools/engine-fixtures-anonymisation.rb
+++ tools/engine-fixtures-anonymisation.rb
@@ -70,6 +70,10 @@ account = JSON.parse(File.read(account_filepath))
 }
 account["billToContact"] = nil
 account["soldToContact"] = {
+    "address1" => account["soldToContact"]["address1"],
+    "address2" => account["soldToContact"]["address2"],
+    "city" => account["soldToContact"]["city"],
+    "zipCode" => account["soldToContact"]["zipCode"],
     "country" => account["soldToContact"]["country"]
 }
 account["gu:sanitised"] = true
