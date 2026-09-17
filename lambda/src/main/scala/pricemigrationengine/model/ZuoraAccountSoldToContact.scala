64fe58b4bda3f3060090f48e3f31a0620aa40f2a
Pascal Honoré <pascal@alseyn.net>
2026-09-17 08:26:15 +0100

    test: remove potentially useless import

--- lambda/src/main/scala/pricemigrationengine/model/ZuoraAccountSoldToContact.scala
+++ lambda/src/main/scala/pricemigrationengine/model/ZuoraAccountSoldToContact.scala
@@ -1,6 +1,5 @@
 package pricemigrationengine.model
 
-import pricemigrationengine.model.OptionReader // not sure why this import is needed as should be visible implicitly
 import upickle.default._
 
 case class ZuoraAccountSoldToContact(
--- lambda/src/main/scala/pricemigrationengine/model/ZuoraSubscription.scala
+++ lambda/src/main/scala/pricemigrationengine/model/ZuoraSubscription.scala
@@ -2,7 +2,6 @@ package pricemigrationengine.model
 
 import java.time.LocalDate
 
-import pricemigrationengine.model.OptionReader // not sure why this import is needed as should be visible implicitly
 import upickle.default._
 
 case class ZuoraSubscription(
