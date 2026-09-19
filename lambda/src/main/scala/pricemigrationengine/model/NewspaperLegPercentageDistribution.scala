9a72fa2663228c95d2779cf59c66002cf3eed8ee
Pascal Honoré <pascal@alseyn.net>
2026-09-19 22:50:25 +0100

    NewspaperLegPercentageDistribution update

--- lambda/src/main/scala/pricemigrationengine/model/NewspaperLegPercentageDistribution.scala
+++ lambda/src/main/scala/pricemigrationengine/model/NewspaperLegPercentageDistribution.scala
@@ -43,6 +43,42 @@ object NewspaperLegPercentageDistribution {
       T4xLegPercentage(T1xFriday, BigDecimal(13.2)),
       T4xLegPercentage(T1xSaturday, BigDecimal(17.0)),
       T4xLegPercentage(T1xSunday, BigDecimal(17.0))
+    ),
+    (T3xNewspaperNationalDelivery, T2xSixDay) -> List(
+      T4xLegPercentage(T1xMonday, BigDecimal(15.9)),
+      T4xLegPercentage(T1xTuesday, BigDecimal(15.9)),
+      T4xLegPercentage(T1xWednesday, BigDecimal(15.9)),
+      T4xLegPercentage(T1xThursday, BigDecimal(15.9)),
+      T4xLegPercentage(T1xFriday, BigDecimal(15.9)),
+      T4xLegPercentage(T1xSaturday, BigDecimal(20.5)),
+    ),
+    (T3xNewspaperNationalDelivery, T2xWeekend) -> List(
+      T4xLegPercentage(T1xSaturday, BigDecimal(50.0)),
+      T4xLegPercentage(T1xSunday, BigDecimal(50.0))
+    ),
+    (T3xNewspaperNationalDelivery, T2xEverydayPlus) -> List(
+      T4xLegPercentage(T1xMonday, BigDecimal(11.4)),
+      T4xLegPercentage(T1xTuesday, BigDecimal(11.4)),
+      T4xLegPercentage(T1xWednesday, BigDecimal(11.4)),
+      T4xLegPercentage(T1xThursday, BigDecimal(11.4)),
+      T4xLegPercentage(T1xFriday, BigDecimal(11.4)),
+      T4xLegPercentage(T1xSaturday, BigDecimal(14.7)),
+      T4xLegPercentage(T1xSunday, BigDecimal(14.7)),
+      T4xLegPercentage(T1xDigitalPack, BigDecimal(13.6)),
+    ),
+    (T3xNewspaperNationalDelivery, T2xSixDayPlus) -> List(
+      T4xLegPercentage(T1xMonday, BigDecimal(13.4)),
+      T4xLegPercentage(T1xTuesday, BigDecimal(13.4)),
+      T4xLegPercentage(T1xWednesday, BigDecimal(13.4)),
+      T4xLegPercentage(T1xThursday, BigDecimal(13.4)),
+      T4xLegPercentage(T1xFriday, BigDecimal(13.4)),
+      T4xLegPercentage(T1xSaturday, BigDecimal(17.2)),
+      T4xLegPercentage(T1xDigitalPack, BigDecimal(15.8)),
+    ),
+    (T3xNewspaperNationalDelivery, T2xWeekendPlus) -> List(
+      T4xLegPercentage(T1xSaturday, BigDecimal(34.2)),
+      T4xLegPercentage(T1xSunday, BigDecimal(34.2)),
+      T4xLegPercentage(T1xDigitalPack, BigDecimal(31.6)),
     )
   )
 }
