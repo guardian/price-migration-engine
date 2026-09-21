6801502b27d0bb9873a11ed4e3d103ee23380474
Pascal Honoré <pascal@alseyn.net>
2026-09-21 12:37:56 +0100

    T5x

--- lambda/src/main/scala/pricemigrationengine/model/GuardianWeeklyLegPercentageDistribution.scala
+++ lambda/src/main/scala/pricemigrationengine/model/GuardianWeeklyLegPercentageDistribution.scala
@@ -8,64 +8,64 @@ package pricemigrationengine.model
 // - Semi Annual
 // - Annual
 // - "6 for 6"
-sealed trait GuardianWeeklyDeliverySchedule
-object GWDSMonth extends GuardianWeeklyDeliverySchedule
-object GWDSQuarter extends GuardianWeeklyDeliverySchedule
-object GWDSemiAnnual extends GuardianWeeklyDeliverySchedule
-object GWDAnnual extends GuardianWeeklyDeliverySchedule
-object GWD6For6 extends GuardianWeeklyDeliverySchedule
+sealed trait T4xGuardianWeeklyDeliverySchedule
+object T4xMonth extends T4xGuardianWeeklyDeliverySchedule
+object T4xQuarter extends T4xGuardianWeeklyDeliverySchedule
+object T4xSemiAnnual extends T4xGuardianWeeklyDeliverySchedule
+object T4xAnnual extends T4xGuardianWeeklyDeliverySchedule
+object T4x6For6 extends T4xGuardianWeeklyDeliverySchedule
 
-case class GWDistribution(gardianWeeklyLeg: BigDecimal, digitalPackLeg: BigDecimal)
+case class T5xDistribution(gardianWeeklyLeg: BigDecimal, digitalPackLeg: BigDecimal)
 
 object GuardianWeeklyLegPercentageDistribution {
-  val monthDistributions: Map[(Currency, PricingLocalisation), GWDistribution] = Map(
-    ("GPB", Domestic) -> GWDistribution(BigDecimal(60.5), BigDecimal(39.5)),
-    ("EUR", Domestic) -> GWDistribution(BigDecimal(67.9), BigDecimal(32.1)),
-    ("USD", RestOfWorld) -> GWDistribution(BigDecimal(57.7), BigDecimal(42.3)),
-    ("GBP", RestOfWorld) -> GWDistribution(BigDecimal(58.4), BigDecimal(41.6)),
-    ("USD", Domestic) -> GWDistribution(BigDecimal(65.1), BigDecimal(34.9)),
-    ("CAD", Domestic) -> GWDistribution(BigDecimal(60.9), BigDecimal(39.1)),
-    ("AUD", Domestic) -> GWDistribution(BigDecimal(66.5), BigDecimal(33.5)),
-    ("NZD", Domestic) -> GWDistribution(BigDecimal(68.7), BigDecimal(31.3))
+  val monthDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
+    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
+    ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
+    ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
+    ("USD", Domestic) -> T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)),
+    ("CAD", Domestic) -> T5xDistribution(BigDecimal(60.9), BigDecimal(39.1)),
+    ("AUD", Domestic) -> T5xDistribution(BigDecimal(66.5), BigDecimal(33.5)),
+    ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
-  val quarterlyDistributions: Map[(Currency, PricingLocalisation), GWDistribution] = Map(
-    ("GPB", Domestic) -> GWDistribution(BigDecimal(60.5), BigDecimal(39.5)),
-    ("EUR", Domestic) -> GWDistribution(BigDecimal(67.9), BigDecimal(32.1)),
-    ("USD", RestOfWorld) -> GWDistribution(BigDecimal(57.7), BigDecimal(42.3)),
-    ("GBP", RestOfWorld) -> GWDistribution(BigDecimal(58.4), BigDecimal(41.6)),
-    ("USD", Domestic) -> GWDistribution(BigDecimal(65.1), BigDecimal(34.9)),
-    ("CAD", Domestic) -> GWDistribution(BigDecimal(60.9), BigDecimal(39.1)),
-    ("AUD", Domestic) -> GWDistribution(BigDecimal(66.5), BigDecimal(33.5)),
-    ("NZD", Domestic) -> GWDistribution(BigDecimal(68.7), BigDecimal(31.3))
+  val quarterlyDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
+    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
+    ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
+    ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
+    ("USD", Domestic) -> T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)),
+    ("CAD", Domestic) -> T5xDistribution(BigDecimal(60.9), BigDecimal(39.1)),
+    ("AUD", Domestic) -> T5xDistribution(BigDecimal(66.5), BigDecimal(33.5)),
+    ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
-  val semiAnnualDistributions: Map[(Currency, PricingLocalisation), GWDistribution] = Map(
-    ("GPB", Domestic) -> GWDistribution(BigDecimal(60.5), BigDecimal(39.5)),
-    ("EUR", Domestic) -> GWDistribution(BigDecimal(67.9), BigDecimal(32.1)),
-    ("USD", RestOfWorld) -> GWDistribution(BigDecimal(57.7), BigDecimal(42.3)),
-    ("GBP", RestOfWorld) -> GWDistribution(BigDecimal(58.4), BigDecimal(41.6)),
-    ("USD", Domestic) -> GWDistribution(BigDecimal(65.1), BigDecimal(34.9)),
-    ("CAD", Domestic) -> GWDistribution(BigDecimal(60.9), BigDecimal(39.1)),
-    ("AUD", Domestic) -> GWDistribution(BigDecimal(66.5), BigDecimal(33.5)),
-    ("NZD", Domestic) -> GWDistribution(BigDecimal(68.7), BigDecimal(31.3))
+  val semiAnnualDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
+    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
+    ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
+    ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
+    ("USD", Domestic) -> T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)),
+    ("CAD", Domestic) -> T5xDistribution(BigDecimal(60.9), BigDecimal(39.1)),
+    ("AUD", Domestic) -> T5xDistribution(BigDecimal(66.5), BigDecimal(33.5)),
+    ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
-  val annualDistributions: Map[(Currency, PricingLocalisation), GWDistribution] = Map(
-    ("GPB", Domestic) -> GWDistribution(BigDecimal(64.8), BigDecimal(35.2)),
-    ("EUR", Domestic) -> GWDistribution(BigDecimal(71.7), BigDecimal(28.3)),
-    ("USD", RestOfWorld) -> GWDistribution(BigDecimal(62.1), BigDecimal(37.9)),
-    ("GBP", RestOfWorld) -> GWDistribution(BigDecimal(62.8), BigDecimal(37.2)),
-    ("USD", Domestic) -> GWDistribution(BigDecimal(69.2), BigDecimal(30.8)),
-    ("CAD", Domestic) -> GWDistribution(BigDecimal(65.1), BigDecimal(34.9)),
-    ("AUD", Domestic) -> GWDistribution(BigDecimal(70.4), BigDecimal(29.6)),
-    ("NZD", Domestic) -> GWDistribution(BigDecimal(72.5), BigDecimal(27.5))
+  val annualDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
+    ("GPB", Domestic) -> T5xDistribution(BigDecimal(64.8), BigDecimal(35.2)),
+    ("EUR", Domestic) -> T5xDistribution(BigDecimal(71.7), BigDecimal(28.3)),
+    ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(62.1), BigDecimal(37.9)),
+    ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(62.8), BigDecimal(37.2)),
+    ("USD", Domestic) -> T5xDistribution(BigDecimal(69.2), BigDecimal(30.8)),
+    ("CAD", Domestic) -> T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)),
+    ("AUD", Domestic) -> T5xDistribution(BigDecimal(70.4), BigDecimal(29.6)),
+    ("NZD", Domestic) -> T5xDistribution(BigDecimal(72.5), BigDecimal(27.5))
   )
-  val SixForSixDistributions: Map[(Currency, PricingLocalisation), GWDistribution] = Map(
-    ("GPB", Domestic) -> GWDistribution(BigDecimal(60.5), BigDecimal(39.5)),
-    ("EUR", Domestic) -> GWDistribution(BigDecimal(67.9), BigDecimal(32.1)),
-    ("USD", RestOfWorld) -> GWDistribution(BigDecimal(57.7), BigDecimal(42.3)),
-    ("GBP", RestOfWorld) -> GWDistribution(BigDecimal(58.4), BigDecimal(41.6)),
-    ("USD", Domestic) -> GWDistribution(BigDecimal(65.1), BigDecimal(34.9)),
-    ("CAD", Domestic) -> GWDistribution(BigDecimal(60.9), BigDecimal(39.1)),
-    ("AUD", Domestic) -> GWDistribution(BigDecimal(66.5), BigDecimal(33.5)),
-    ("NZD", Domestic) -> GWDistribution(BigDecimal(68.7), BigDecimal(31.3))
+  val SixForSixDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
+    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
+    ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
+    ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
+    ("USD", Domestic) -> T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)),
+    ("CAD", Domestic) -> T5xDistribution(BigDecimal(60.9), BigDecimal(39.1)),
+    ("AUD", Domestic) -> T5xDistribution(BigDecimal(66.5), BigDecimal(33.5)),
+    ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
 }
