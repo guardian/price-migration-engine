d11131d39c373b4861cde2a4d91f40a81837f7a4
Pascal Honoré <pascal@alseyn.net>
2026-09-21 17:42:41 +0100

    tests

--- lambda/src/test/scala/pricemigrationengine/model/GuardianWeeklyLegPercentageDistributionTest.scala
+++ lambda/src/test/scala/pricemigrationengine/model/GuardianWeeklyLegPercentageDistributionTest.scala
@@ -3,11 +3,25 @@ package pricemigrationengine.model
 class GuardianWeeklyLegPercentageDistributionTest extends munit.FunSuite {
   test("getDistribution") {
     assertEquals(
+      GuardianWeeklyLegPercentageDistribution.getDistribution(T4xAnnual, "GBP", RestOfWorld),
+      Some(T5xDistribution(BigDecimal(62.8), BigDecimal(37.2)))
+    )
+  }
+  test("getDistribution") {
+    assertEquals(
       GuardianWeeklyLegPercentageDistribution.getDistribution(T4xMonth, "USD", Domestic),
       Some(T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)))
     )
     assertEquals(
-      GuardianWeeklyLegPercentageDistribution.getDistribution(T4xMonth, "CAD", Domestic),
+      GuardianWeeklyLegPercentageDistribution.getDistribution(T4xQuarter, "CAD", Domestic),
+      Some(T5xDistribution(BigDecimal(60.9), BigDecimal(39.1)))
+    )
+    assertEquals(
+      GuardianWeeklyLegPercentageDistribution.getDistribution(T4xAnnual, "CAD", Domestic),
+      Some(T5xDistribution(BigDecimal(65.1), BigDecimal(34.9)))
+    )
+    assertEquals(
+      GuardianWeeklyLegPercentageDistribution.getDistribution(T4x6For6, "CAD", Domestic),
       Some(T5xDistribution(BigDecimal(60.9), BigDecimal(39.1)))
     )
   }
