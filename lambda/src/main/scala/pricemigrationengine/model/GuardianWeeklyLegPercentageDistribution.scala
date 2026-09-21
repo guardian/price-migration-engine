b55707332e87d8da05666c96657026335bd53fd6
Pascal Honoré <pascal@alseyn.net>
2026-09-21 16:15:42 +0100

    currency name correction: GBP

--- lambda/src/main/scala/pricemigrationengine/model/GuardianWeeklyLegPercentageDistribution.scala
+++ lambda/src/main/scala/pricemigrationengine/model/GuardianWeeklyLegPercentageDistribution.scala
@@ -19,7 +19,7 @@ case class T5xDistribution(gardianWeeklyLeg: BigDecimal, digitalPackLeg: BigDeci
 
 object GuardianWeeklyLegPercentageDistribution {
   val monthDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
-    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("GBP", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
     ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
     ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
     ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
@@ -29,7 +29,7 @@ object GuardianWeeklyLegPercentageDistribution {
     ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
   val quarterlyDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
-    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("GBP", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
     ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
     ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
     ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
@@ -39,7 +39,7 @@ object GuardianWeeklyLegPercentageDistribution {
     ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
   val semiAnnualDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
-    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("GBP", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
     ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
     ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
     ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
@@ -49,7 +49,7 @@ object GuardianWeeklyLegPercentageDistribution {
     ("NZD", Domestic) -> T5xDistribution(BigDecimal(68.7), BigDecimal(31.3))
   )
   val annualDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
-    ("GPB", Domestic) -> T5xDistribution(BigDecimal(64.8), BigDecimal(35.2)),
+    ("GBP", Domestic) -> T5xDistribution(BigDecimal(64.8), BigDecimal(35.2)),
     ("EUR", Domestic) -> T5xDistribution(BigDecimal(71.7), BigDecimal(28.3)),
     ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(62.1), BigDecimal(37.9)),
     ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(62.8), BigDecimal(37.2)),
@@ -59,7 +59,7 @@ object GuardianWeeklyLegPercentageDistribution {
     ("NZD", Domestic) -> T5xDistribution(BigDecimal(72.5), BigDecimal(27.5))
   )
   val SixForSixDistributions: Map[(Currency, PricingLocalisation), T5xDistribution] = Map(
-    ("GPB", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
+    ("GBP", Domestic) -> T5xDistribution(BigDecimal(60.5), BigDecimal(39.5)),
     ("EUR", Domestic) -> T5xDistribution(BigDecimal(67.9), BigDecimal(32.1)),
     ("USD", RestOfWorld) -> T5xDistribution(BigDecimal(57.7), BigDecimal(42.3)),
     ("GBP", RestOfWorld) -> T5xDistribution(BigDecimal(58.4), BigDecimal(41.6)),
