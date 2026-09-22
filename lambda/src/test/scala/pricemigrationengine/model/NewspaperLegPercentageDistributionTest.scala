b246aceb825fec81f132f3f503b69a31c4c9cb88
Pascal Honoré <pascal@alseyn.net>
2026-09-22 10:27:53 +0100

    use correct test

--- lambda/src/test/scala/pricemigrationengine/model/NewspaperLegPercentageDistributionTest.scala
+++ lambda/src/test/scala/pricemigrationengine/model/NewspaperLegPercentageDistributionTest.scala
@@ -3,8 +3,17 @@ package pricemigrationengine.model
 class NewspaperLegPercentageDistributionTest extends munit.FunSuite {
   test("getDistribution") {
     assertEquals(
-      GuardianWeeklyLegPercentageDistribution.getDistribution(T4xAnnual, "GBP", RestOfWorld),
-      Some(T5xDistribution(BigDecimal(62.8), BigDecimal(37.2)))
+      NewspaperLegPercentageDistribution.getDistribution(T3xNewspaperNationalDelivery, T2xSixDay),
+      Some(
+        List(
+          T4xLegPercentage(T1xMonday, BigDecimal(15.9)),
+          T4xLegPercentage(T1xTuesday, BigDecimal(15.9)),
+          T4xLegPercentage(T1xWednesday, BigDecimal(15.9)),
+          T4xLegPercentage(T1xThursday, BigDecimal(15.9)),
+          T4xLegPercentage(T1xFriday, BigDecimal(15.9)),
+          T4xLegPercentage(T1xSaturday, BigDecimal(20.5)),
+        )
+      )
     )
   }
   test("Percentages sums to 100%, newspaperNationalDeliveryLegPercentageMapping") {
